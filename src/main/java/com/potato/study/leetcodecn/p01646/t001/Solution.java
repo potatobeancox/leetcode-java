e5aecfd8112880a85f3d0224da8928f295b16152
liuzhao11 <liuzhao11@sankuai.com>
2021-08-14 16:18:39 +0800

    2021.8.14 1646. 获取生成数组中的最大值 2022.11.22

---
+++ src/main/java/com/potato/study/leetcodecn/p01646/t001/Solution.java
@@ -0,0 +1,87 @@
+package com.potato.study.leetcodecn.p01646.t001;
+
+import org.junit.Assert;
+
+import java.util.*;
+
+/**
+ * 1646. 获取生成数组中的最大值
+ *
+ * 给你一个整数 n 。按下述规则生成一个长度为 n + 1 的数组 nums ：
+
+ nums[0] = 0
+ nums[1] = 1
+ 当 2 <= 2 * i <= n 时，nums[2 * i] = nums[i]
+ 当 2 <= 2 * i + 1 <= n 时，nums[2 * i + 1] = nums[i] + nums[i + 1]
+ 返回生成数组 nums 中的 最大 值。
+
+  
+
+ 示例 1：
+
+ 输入：n = 7
+ 输出：3
+ 解释：根据规则：
+ nums[0] = 0
+ nums[1] = 1
+ nums[(1 * 2) = 2] = nums[1] = 1
+ nums[(1 * 2) + 1 = 3] = nums[1] + nums[2] = 1 + 1 = 2
+ nums[(2 * 2) = 4] = nums[2] = 1
+ nums[(2 * 2) + 1 = 5] = nums[2] + nums[3] = 1 + 2 = 3
+ nums[(3 * 2) = 6] = nums[3] = 2
+ nums[(3 * 2) + 1 = 7] = nums[3] + nums[4] = 2 + 1 = 3
+ 因此，nums = [0,1,1,2,1,3,2,3]，最大值 3
+ 示例 2：
+
+ 输入：n = 2
+ 输出：1
+ 解释：根据规则，nums[0]、nums[1] 和 nums[2] 之中的最大值是 1
+ 示例 3：
+
+ 输入：n = 3
+ 输出：2
+ 解释：根据规则，nums[0]、nums[1]、nums[2] 和 nums[3] 之中的最大值是 2
+  
+
+ 提示：
+
+ 0 <= n <= 100
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/get-maximum-in-generated-array
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    public int getMaximumGenerated(int n) {
+        int[] nums = new int[n+1];
+        int max = 0;
+        for (int i = 0; i <= n; i++) {
+            if (i == 0) {
+                nums[i] = 0;
+                continue;
+            }
+            if (i == 1) {
+                nums[i] = 1;
+                max = 1;
+                continue;
+            }
+            if (i % 2 == 1) {
+                nums[i] = nums[i/2] + nums[i/2 + 1];
+            } else {
+                nums[i] = nums[i / 2];
+            }
+            max = Math.max(max, nums[i]);
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int n = 7;
+        int maximumGenerated = solution.getMaximumGenerated(n);
+        System.out.println(maximumGenerated);
+        Assert.assertEquals(3, maximumGenerated);
+    }
+}
