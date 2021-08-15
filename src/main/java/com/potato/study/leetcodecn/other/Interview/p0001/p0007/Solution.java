15d20485311797da4f3bbecd5b413e5f7144e492
liuzhao11 <liuzhao11@sankuai.com>
2021-08-15 15:26:45 +0800

    2021.8.15 面试题 01.07. 旋转矩阵 2022.11.30

---
+++ src/main/java/com/potato/study/leetcodecn/other/Interview/p0001/p0007/Solution.java
@@ -0,0 +1,76 @@
+package com.potato.study.leetcodecn.other.Interview.p0001.p0007;
+
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 面试题 01.07. 旋转矩阵
+ *
+ * 给你一幅由 N × N 矩阵表示的图像，其中每个像素的大小为 4 字节。请你设计一种算法，将图像旋转 90 度。
+
+ 不占用额外内存空间能否做到？
+
+  
+
+ 示例 1:
+
+ 给定 matrix =
+ [
+ [1,2,3],
+ [4,5,6],
+ [7,8,9]
+ ],
+
+ 原地旋转输入矩阵，使其变为:
+ [
+ [7,4,1],
+ [8,5,2],
+ [9,6,3]
+ ]
+ 示例 2:
+
+ 给定 matrix =
+ [
+ [ 5, 1, 9,11],
+ [ 2, 4, 8,10],
+ [13, 3, 6, 7],
+ [15,14,12,16]
+ ],
+
+ 原地旋转输入矩阵，使其变为:
+ [
+ [15,13, 2, 5],
+ [14, 3, 4, 1],
+ [12, 6, 8, 9],
+ [16, 7,10,11]
+ ]
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/rotate-matrix-lcci
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+
+    public void rotate(int[][] matrix) {
+        // 上下翻转
+        for (int i = 0; i < matrix.length / 2; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[matrix.length - 1 - i][j];
+                matrix[matrix.length - 1 - i][j] = tmp;
+            }
+        }
+        // 沿着主对角线 翻转
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < i; j++) {
+                int tmp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = tmp;
+            }
+        }
+    }
+}
