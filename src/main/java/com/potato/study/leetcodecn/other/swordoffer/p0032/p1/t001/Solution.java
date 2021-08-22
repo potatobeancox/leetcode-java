7e2d962aedab492ad5eb27d3158fa23889cddf96
liuzhao11 <liuzhao11@sankuai.com>
2021-08-22 23:29:43 +0800

    2021.8.22 剑指 Offer 32 - I. 从上到下打印二叉树 2022.12.16

---
+++ src/main/java/com/potato/study/leetcodecn/other/swordoffer/p0032/p1/t001/Solution.java
@@ -0,0 +1,70 @@
+package com.potato.study.leetcodecn.other.swordoffer.p0032.p1.t001;
+
+import com.potato.study.leetcode.domain.TreeNode;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * 剑指 Offer 32 - I. 从上到下打印二叉树
+ *
+ * 从上到下打印出二叉树的每个节点，同一层的节点按照从左到右的顺序打印。
+
+  
+
+ 例如:
+ 给定二叉树: [3,9,20,null,null,15,7],
+
+ 3
+ / \
+ 9  20
+ /  \
+ 15   7
+ 返回：
+
+ [3,9,20,15,7]
+  
+
+ 提示：
+
+ 节点总数 <= 1000
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/cong-shang-dao-xia-da-yin-er-cha-shu-lcof
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    /**
+     *
+     * @param root
+     * @return
+     */
+    public int[] levelOrder(TreeNode root) {
+        if (null == root) {
+            return new int[0];
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        List<Integer> result = new ArrayList<>();
+        while (!queue.isEmpty()) {
+            TreeNode poll = queue.poll();
+            result.add(poll.val);
+            if (poll.left != null) {
+                queue.add(poll.left);
+            }
+            if (poll.right != null) {
+                queue.add(poll.right);
+            }
+        }
+        int[] arr = new int[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+            arr[i] = result.get(i);
+        }
+        return arr;
+    }
+
+}
