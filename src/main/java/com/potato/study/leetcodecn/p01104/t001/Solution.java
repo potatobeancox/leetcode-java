8cce016037599c6a12381ed6e548144d8babd9e8
liuzhao03 <>
2021-09-25 18:43:32 +0800

    2021.9.25 1104. 二叉树寻路 2023.1.22

--- src/main/java/com/potato/study/leetcodecn/p01104/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p01104/t001/Solution.java
@@ -49,31 +49,69 @@ public class Solution {
      * @return
      */
     public List<Integer> pathInZigZagTree(int label) {
-        List<Integer> list = new ArrayList<>();
-        while (label >= 1) {
-            list.add(label);
-            label /= 2;
-        }
-        // 遍历结果 计算翻转之后的结果 利用满二叉树性质 第k层 开始点 2 ^(k-1)终止点 2^k - 1
-        List<Integer> result = new ArrayList<>();
-
-        for (int i = list.size(); i > 0 ; i--) {
-            if (i % 2 == 0) {
-                int num = (int) (3 * Math.pow(2, (i-1)) - 1);
-                int target = num - list.get(i-1);
-                result.add(target);
-            } else {
-                result.add(list.get(i-1));
+        // 先判断 当前label 在多少层 2^(i-1)  i从1开始 计数
+        int targetLevel = 0;
+        for (int i = 1; i <= label; i++) {
+            if (Math.pow(2, i - 1) <= label && label <= Math.pow(2, i) - 1) {
+                targetLevel = i;
+                break;
             }
         }
-        return result;
+        // 如果当前层数i 是偶数 需要将 当前置换 然后/2 得到下一个层的数字，再根据层数 判定是够需要进行转换
+        List<Integer> result = new ArrayList<>();
+        result.add(label);
+        int count = 0;
+        targetLevel--;
+        int tmp = label;
+        while (targetLevel > 0) {
+            int total = (int) (Math.pow(2, targetLevel) - 1 + Math.pow(2, targetLevel - 1));
+            // 当前数字
+            tmp /= 2;
+            if (count % 2 == 1) {
+                result.add(tmp);
+            } else {
+                result.add(total - tmp);
+            }
+            count++;
+            targetLevel--;
+        }
+        // reverse
+        List<Integer> newResult = new ArrayList<>();
+        for (int i = result.size() - 1; i >= 0; i--) {
+            newResult.add(result.get(i));
+        }
+        return newResult;
     }
+//        List<Integer> list = new ArrayList<>();
+//        while (label >= 1) {
+//            list.add(label);
+//            label /= 2;
+//        }
+//        // 遍历结果 计算翻转之后的结果 利用满二叉树性质 第k层 开始点 2 ^(k-1)终止点 2^k - 1
+//        List<Integer> result = new ArrayList<>();
+//
+//        for (int i = list.size(); i > 0 ; i--) {
+//            if (i % 2 == 0) {
+//                int num = (int) (3 * Math.pow(2, (i-1)) - 1);
+//                int target = num - list.get(i-1);
+//                result.add(target);
+//            } else {
+//                result.add(list.get(i-1));
+//            }
+//        }
+//        return result;
+//    }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
         int label = 14;
         List<Integer> list = solution.pathInZigZagTree(label);
         //[1,3,4,14]
+        System.out.println(list);
+
+        label = 26;
+        list = solution.pathInZigZagTree(label);
+        //[1,2,6,10,26]
         System.out.println(list);
     }
 }
