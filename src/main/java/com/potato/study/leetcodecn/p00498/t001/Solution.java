51c365bb5981e717637a755fe2cd5711b95663d4
liuzhao11 <liuzhao11@sankuai.com>
2021-10-07 15:56:40 +0800

    2021.10.7 2012. 数组美丽值求和 2023.2.19

--- src/main/java/com/potato/study/leetcodecn/p00498/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00498/t001/Solution.java
@@ -1,10 +1,8 @@
 package com.potato.study.leetcodecn.p00498.t001;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Stack;
+import org.junit.Assert;
+
+import java.util.*;
 
 /**
  * 498. 对角线遍历
@@ -48,6 +46,19 @@ public class Solution {
         // 控制当前 ij 的和 从 0开始到  行数 + 列数 -1
         int m = mat.length;
         int n = mat[0].length;
+//        if (m == 1) {
+//            return mat[0];
+//        }
+
+        int[] result = new int[m * n];
+        int current = 0;
+//        if (n == 1) {
+//            for (int i = 0; i < m; i++) {
+//                result[current++] = mat[i][0];
+//            }
+//            return result;
+//        }
+
         int index = 0;
         // 上半部分 遍历第0 行 作为开始节点
         for (int i = 0; i < n; i++) {
@@ -58,25 +69,61 @@ public class Solution {
             }
             // 判断是否需要 reverse 添加
             if (i % 2 == 0) {
-
+                for (int j = list.size() - 1; j >=0 ; j--) {
+                    result[current++] = list.get(j);
+                }
             } else {
-
+                for (int j = 0; j < list.size() ; j++) {
+                    result[current++] = list.get(j);
+                }
             }
-
+            index++;
         }
         // 下半部分
         for (int i = 1; i < m; i++) {
-
-
+            List<Integer> list = new ArrayList<>();
+            for (int start1 = i; start1 < m; start1++) {
+                int start2 = index - start1;
+                list.add(mat[start1][start2]);
+            }
             // 判断是否需要 reverse 添加 偶数需要翻转
             if (i % 2 == 0) {
-
+                for (int j = list.size() - 1; j >=0 ; j--) {
+                    result[current++] = list.get(j);
+                }
             } else {
-
+                for (int j = 0; j < list.size() ; j++) {
+                    result[current++] = list.get(j);
+                }
             }
+            index++;
         }
+        return result;
+    }
 
-        return null;
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] mat = new int[][] {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9},
+        };
+        int[] diagonalOrder = solution.findDiagonalOrder(mat);
+        System.out.println(Arrays.toString(diagonalOrder));
+        Assert.assertArrayEquals(new int[] {
+                1,2,4,7,5,3,6,8,9
+        }, diagonalOrder);
+
+
+        mat = new int[][] {
+                {2,3}
+        };
+        diagonalOrder = solution.findDiagonalOrder(mat);
+        System.out.println(Arrays.toString(diagonalOrder));
+        Assert.assertArrayEquals(new int[] {
+                2,3
+        }, diagonalOrder);
     }
 
 }
