c68fc3819045dcc52af8e8db0bd5a0bae76470c2
liuzhao03 <>
2021-10-12 13:20:02 +0800

    2021.10.9 951. 翻转等价二叉树 2023.3.16

---
+++ src/main/java/com/potato/study/leetcodecn/p00583/t001/Solution.java
@@ -0,0 +1,58 @@
+package com.potato.study.leetcodecn.p00583.t001;
+
+
+import java.util.Arrays;
+
+/**
+ * 583. 两个字符串的删除操作
+ *
+ * 给定两个单词 word1 和 word2，找到使得 word1 和 word2 相同所需的最小步数，每步可以删除任意一个字符串中的一个字符。
+ *
+ *  
+ *
+ * 示例：
+ *
+ * 输入: "sea", "eat"
+ * 输出: 2
+ * 解释: 第一步将"sea"变为"ea"，第二步将"eat"变为"ea"
+ *  
+ *
+ * 提示：
+ *
+ * 给定单词的长度不超过500。
+ * 给定单词中的字符只含有小写字母。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/delete-operation-for-two-strings
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    // 583
+    public int minDistance(String word1, String word2) {
+        // 其实是求最长公共子需求 然后用两个长度把最长公共子序列长度刨除
+        if (word1 == null && word2 == null) {
+            return 0;
+        } else if (word1 == null) {
+            return word2.length();
+        } else if (word2 == null) {
+            return word1.length();
+        }
+        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        // dp ij  dp 00 = 0  i-1 j-1 字符串以前 最长的公共子序列长度
+        dp[0][0] = 0;
+        for (int i = 1; i < word1.length() + 1; i++) {
+            for (int j = 1; j < word2.length() + 1; j++) {
+                // 如果 i-1 j-1 相同 直接 缩小到 ，如果不相同 就是max i-1 j i j-1
+                if (word1.charAt(i-1) == word2.charAt(j-1)) {
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                } else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return word1.length() + word2.length() - dp[word1.length()][word2.length()];
+    }
+
+}
