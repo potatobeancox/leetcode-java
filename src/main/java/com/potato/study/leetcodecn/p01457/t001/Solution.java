a0ba34e1a89224ff7832537a25cafa2e10148115
liuzhao11 <liuzhao11@sankuai.com>
2021-10-17 23:15:16 +0800

    2021.10.17 1457. 二叉树中的伪回文路径 2023.3.28

--- src/main/java/com/potato/study/leetcodecn/p01457/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p01457/t001/Solution.java
@@ -1,6 +1,10 @@
 package com.potato.study.leetcodecn.p01457.t001;
 
 import com.potato.study.leetcode.domain.TreeNode;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 1457. 二叉树中的伪回文路径
@@ -45,7 +49,73 @@ import com.potato.study.leetcode.domain.TreeNode;
  */
 public class Solution {
 
+    private int count;
+
     public int pseudoPalindromicPaths (TreeNode root) {
-        return -1;
+        if (null == root) {
+            return 0;
+        }
+        this.count = 0;
+        // dfs 遍历 root 生成 结果
+        int bit = 0;
+        dfs(bit, root);
+        return count;
+    }
+
+
+    /**
+     * 判断的使用 将状态压缩到一个 int形中 每个位置做 异或 代表数字是否是奇数
+     * @param bit 状态
+     * @param node
+     */
+    private void dfs(int bit, TreeNode node) {
+        // 当前节点放入builder 中 dfs 往后找 左右孩子
+        bit ^= (1 << node.val);
+        // 终止条件 node == null
+        if (node.left == null && node.right == null) {
+            if (isPalindrome(bit)) {
+                this.count++;
+            }
+            return;
+        }
+        if (node.left != null) {
+            dfs(bit, node.left);
+        }
+        if (node.right != null) {
+            dfs(bit, node.right);
+        }
+    }
+
+
+
+    /**
+     * 判断单词是不是 伪回文 数字
+     * @param bit
+     * @return
+     */
+    private boolean isPalindrome(int bit) {
+        // 偶数
+        if (bit == 0) {
+            return true;
+        }
+        // 是不是只有一个位置是1
+        if ((bit & (bit - 1)) == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode root = new TreeNode(2);
+        root.left = new TreeNode(3);
+        root.left.left = new TreeNode(3);
+        root.left.right = new TreeNode(1);
+        root.right = new TreeNode(1);
+        root.right.right = new TreeNode(1);
+
+        int i = solution.pseudoPalindromicPaths(root);
+        System.out.println(i);
+        Assert.assertEquals(2, i);
     }
 }
