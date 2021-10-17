61516ee90c22c73051f6cbf8598e3c4a3762c9b5
liuzhao11 <liuzhao11@sankuai.com>
2021-10-17 22:29:03 +0800

    2021.10.12 1238. 循环码排列 2023.3.27

---
+++ src/main/java/com/potato/study/leetcodecn/p00233/t001/Solution.java
@@ -0,0 +1,36 @@
+package com.potato.study.leetcodecn.p00233.t001;
+
+import java.util.Stack;
+
+/**
+ * 233. 数字 1 的个数
+ *
+ * 给定一个整数 n，计算所有小于等于 n 的非负整数中数字 1 出现的个数。
+
+  
+
+ 示例 1：
+
+ 输入：n = 13
+ 输出：6
+ 示例 2：
+
+ 输入：n = 0
+ 输出：0
+  
+
+ 提示：
+
+ 0 <= n <= 109
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/number-of-digit-one
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+    public int countDigitOne(int n) {
+
+        return -1;
+    }
+}
