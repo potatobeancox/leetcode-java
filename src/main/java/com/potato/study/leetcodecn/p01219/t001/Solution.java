da2fc5ae6aa837e1b358e07179c32e1702b00964
liuzhao03 <>
2021-10-24 14:15:46 +0800

    2021.10.24 1219. 黄金矿工 2023.4.19

---
+++ src/main/java/com/potato/study/leetcodecn/p01219/t001/Solution.java
@@ -0,0 +1,104 @@
+package com.potato.study.leetcodecn.p01219.t001;
+
+
+/**
+ * 1219. 黄金矿工
+ *
+ * 你要开发一座金矿，地质勘测学家已经探明了这座金矿中的资源分布，并用大小为 m * n 的网格 grid 进行了标注。每个单元格中的整数就表示这一单元格中的黄金数量；如果该单元格是空的，那么就是 0。
+ *
+ * 为了使收益最大化，矿工需要按以下规则来开采黄金：
+ *
+ * 每当矿工进入一个单元，就会收集该单元格中的所有黄金。
+ * 矿工每次可以从当前位置向上下左右四个方向走。
+ * 每个单元格只能被开采（进入）一次。
+ * 不得开采（进入）黄金数目为 0 的单元格。
+ * 矿工可以从网格中 任意一个 有黄金的单元格出发或者是停止。
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：grid = [[0,6,0],[5,8,7],[0,9,0]]
+ * 输出：24
+ * 解释：
+ * [[0,6,0],
+ *  [5,8,7],
+ *  [0,9,0]]
+ * 一种收集最多黄金的路线是：9 -> 8 -> 7。
+ * 示例 2：
+ *
+ * 输入：grid = [[1,0,7],[2,0,6],[3,4,5],[0,3,0],[9,0,20]]
+ * 输出：28
+ * 解释：
+ * [[1,0,7],
+ *  [2,0,6],
+ *  [3,4,5],
+ *  [0,3,0],
+ *  [9,0,20]]
+ * 一种收集最多黄金的路线是：1 -> 2 -> 3 -> 4 -> 5 -> 6 -> 7。
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= grid.length, grid[i].length <= 15
+ * 0 <= grid[i][j] <= 100
+ * 最多 25 个单元格中有黄金。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/path-with-maximum-gold
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    private int maxGold;
+    private int[][] direction;
+    /**
+     * 从每个位置开始进行dfs 记录最大位置
+     * @param grid
+     * @return
+     */
+    public int getMaximumGold(int[][] grid) {
+        this.maxGold = 0;
+        this.direction = new int[][] {
+                {-1, 0},
+                {1, 0},
+                {0, 1},
+                {0, -1}
+        };
+        boolean[][] visit = new boolean[grid.length][grid[0].length];
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 0) {
+                    continue;
+                }
+                dfs(grid, i, j, visit, 0);
+            }
+        }
+        return maxGold;
+    }
+
+    private void dfs(int[][] grid, int i, int j, boolean[][] visit, int currentValue) {
+        // 终止条件
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length) {
+            return;
+        }
+        if (grid[i][j] == 0) {
+            return;
+        }
+        if (visit[i][j]) {
+            return;
+        }
+        // 访问当前点 并更新 max
+        visit[i][j] = true;
+        currentValue += grid[i][j];
+        maxGold = Math.max(maxGold, currentValue);
+        // dfs
+        for (int k = 0; k < direction.length; k++) {
+            int di = i + direction[k][0];
+            int dj = j + direction[k][1];
+            dfs(grid, di, dj, visit, currentValue);
+        }
+        // visit 不访问
+        visit[i][j] = false;
+    }
+}
