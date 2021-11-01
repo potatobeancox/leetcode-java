0d95537d3efdc06c46aa68132179a4ee4d312403
liuzhao11 <liuzhao11@sankuai.com>
2021-11-01 22:29:25 +0800

    2021.11.1 剑指 Offer 65. 不用加减乘除做加法 2023.6.1

---
+++ src/main/java/com/potato/study/leetcodecn/other/swordoffer/p0068/p1/t001/Solution.java
@@ -0,0 +1,50 @@
+package com.potato.study.leetcodecn.other.swordoffer.p0068.p1.t001;
+
+
+import com.potato.study.leetcode.domain.TreeNode;
+
+/**
+ * 剑指 Offer 68 - I. 二叉搜索树的最近公共祖先
+ *
+ * 求 1+2+...+n ，要求不能使用乘除法、for、while、if、else、switch、case等关键字及条件判断语句（A?B:C）。
+
+  
+
+ 示例 1：
+
+ 输入: n = 3
+ 输出: 6
+ 示例 2：
+
+ 输入: n = 9
+ 输出: 45
+  
+
+ 限制：
+
+ 1 <= n <= 10000
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/qiu-12n-lcof
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    /**
+     * 后续遍历
+     * @param root
+     * @param p
+     * @param q
+     * @return
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        return null;
+    }
+
+
+
+
+
+}
