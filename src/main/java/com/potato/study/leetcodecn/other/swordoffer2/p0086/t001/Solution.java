d587ff8316fca93b5ec3fe0cd5762f6ecbd2db93
liuzhao11 <liuzhao11@sankuai.com>
2021-11-07 16:26:09 +0800

    2021.11.7 剑指 Offer II 086. 分割回文子字符串 2023.6.4

--- src/main/java/com/potato/study/leetcodecn/other/swordoffer2/p0086/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/other/swordoffer2/p0086/t001/Solution.java
@@ -1,6 +1,7 @@
 package com.potato.study.leetcodecn.other.swordoffer2.p0086.t001;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -39,6 +40,73 @@ import java.util.List;
 public class Solution {
 
     public String[][] partition(String s) {
-        return null;
+        // 预处理 dp ij ij之间字符串是否是 回文
+        boolean[][] isPalindrome = new boolean[s.length()][s.length()];
+        // 如果 ij位置等 那么 && 里边的
+        for (int i = s.length() - 1; i >= 0; i--) {
+            for (int j = i; j < s.length(); j++) {
+                if (i == j) {
+                    isPalindrome[i][j] = true;
+                    continue;
+                }
+                if (s.charAt(i) != s.charAt(j)) {
+                    isPalindrome[i][j] = false;
+                    continue;
+                }
+                // 字母相同
+                if (i+1 <= j-1) {
+                    isPalindrome[i][j] = isPalindrome[i+1][j-1];
+                } else {
+                    isPalindrome[i][j] = true;
+                }
+            }
+        }
+
+        // dfs 获取结果
+
+        List<List<String>> resultList = new ArrayList<>();
+        dfs(s, 0, isPalindrome, resultList, new ArrayList<>());
+
+        String[][] result = new String[resultList.size()][];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = new String[resultList.get(i).size()];
+            for (int j = 0; j < result[i].length; j++) {
+                result[i][j] = resultList.get(i).get(j);
+            }
+        }
+
+        return result;
     }
+
+
+    private void dfs(String s, int index, boolean[][] isPalindrome, List<List<String>> resultList, List<String> current) {
+        // 终止条件 到头了
+        if (index == s.length()) {
+            if (current.size() != 0) {
+                resultList.add(current);
+            }
+            return;
+        }
+        // 遍历下一个位置 
+        for (int i = index; i < s.length(); i++) {
+            // 不是回文 continue
+            if (!isPalindrome[index][i]) {
+                continue;
+            }
+            // 是 回文
+            String substring = s.substring(index, i + 1);
+            List<String> list = new ArrayList<>(current);
+            list.add(substring);
+            dfs(s, i + 1, isPalindrome, resultList, list);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String input = "google";
+        String[][] partition = solution.partition(input);
+        System.out.println(Arrays.deepToString(partition));
+    }
+
 }
