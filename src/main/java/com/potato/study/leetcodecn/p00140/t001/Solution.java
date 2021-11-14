cfcce8d9e7cc4b07aa06fb0538964eabc064c1ed
liuzhao11 <liuzhao11@sankuai.com>
2021-11-14 22:44:10 +0800

    2021.11.14 140. 单词拆分 II 2023.6.24

--- src/main/java/com/potato/study/leetcodecn/p00140/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00140/t001/Solution.java
@@ -2,8 +2,7 @@ package com.potato.study.leetcodecn.p00140.t001;
 
 import com.google.common.collect.Lists;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
  * 140. 单词拆分 II
@@ -58,42 +57,77 @@ public class Solution {
      * result i =  wordDict 中每个单词 word i- word.len  result[i- word.len] 并集
      *
      * 返回 result.get(s.len -1 )
+     *
+     *
+     * https://leetcode-cn.com/problems/word-break-ii/solution/dan-ci-chai-fen-ii-by-leetcode-solution/
+     *
+     *
      * @param s
      * @param wordDict
      * @return
      */
     public List<String> wordBreak(String s, List<String> wordDict) {
-        List<String>[] result = new List[s.length()];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = new ArrayList<>();
+        // 递归找 从每个位置开始往后找 找到对应的 这个位置开始的对应前缀 如果前缀在word dic 中 递归往后找
+        Set<String> wordDictSet = new HashSet<>(wordDict);
+        Map<Integer, List<List<String>>> indexResultMap = new HashMap<>();
+        getWordBreak(s, wordDictSet, 0, indexResultMap);
+        List<List<String>> lists = indexResultMap.get(0);
+
+        List<String> resultList = new ArrayList<>();
+        for (List<String> eachResult : lists) {
+            resultList.add(String.join(" ", eachResult));
         }
-        for (int i = 0; i < s.length(); i++) {
-            for (String word : wordDict) {
-                // 超出了 0-i的长度
-                if (i + 1 < word.length()) {
-                    continue;
+        return resultList;
+    }
+
+    /**
+     *
+     * @param s
+     * @param wordDictSet
+     * @param startIndex 当前开始位置
+     * @param indexResultMap
+     * @return
+     */
+    private List<List<String>> getWordBreak(String s, Set<String> wordDictSet, int startIndex,
+                                            Map<Integer, List<List<String>>> indexResultMap) {
+        if (startIndex == s.length()) {
+            return new ArrayList<>();
+        }
+        // 如果从index开始找的结果之前找过 直接返回
+        if (indexResultMap.containsKey(startIndex)) {
+            return indexResultMap.get(startIndex);
+        }
+
+        List<List<String>> currentResultList = new ArrayList<>();
+
+        // 不存在 startIndex 的位置 遍历 s 中从 startIndex 开始一直到结尾的每种可能
+        for (int endIndex = startIndex + 1; endIndex <= s.length(); endIndex++) {
+            // 找到当前单词
+            String prefix = s.substring(startIndex, endIndex);
+            // 判断是否在 wordDict 中
+            if (!wordDictSet.contains(prefix)) {
+                continue;
+            }
+            // 获取之后的情况
+            List<List<String>> nextWordBreak = getWordBreak(s, wordDictSet, endIndex, indexResultMap);
+            // 如果之后有结果那就直接拼接 没有结果就拉倒
+            if (nextWordBreak.size() == 0) {
+                if (endIndex == s.length()) {
+                    List<String> res = new ArrayList<>();
+                    res.add(prefix);
+                    currentResultList.add(res);
                 }
-                String target = s.substring(i + 1 - word.length(), i + 1);
-                // 对不上
-                if (!target.equals(word)) {
-                    continue;
-                }
-                if (i + 1 - word.length() == 0) {
-                    // 对上了 看是否是第一个 或者是并
-                    result[i].add(new String(word));
-                } else {
-                    // 不是第一个对上的单词 要看 之前有没有结果，没有也不行
-                    if (result[i - word.length()].size() == 0) {
-                        continue;
-                    }
-                    // 获取到之前的结果 生成新的结果 合并入目前的集合
-                    for (String temp : result[i - word.length()]) {
-                        result[i].add(temp + " " + target);
-                    }
+            } else {
+                // 生成新结果
+                for (List<String> result : nextWordBreak) {
+                    List<String> tmp = new ArrayList<>(result);
+                    tmp.add(0, prefix);
+                    currentResultList.add(tmp);
                 }
             }
         }
-        return result[s.length() - 1];
+        indexResultMap.put(startIndex, currentResultList);
+        return currentResultList;
     }
 
     public static void main(String[] args) {
@@ -102,6 +136,8 @@ public class Solution {
         List<String> wordDict = Lists.newArrayList("cat", "cats", "and", "sand", "dog");
         List<String> list = solution.wordBreak(s, wordDict);
         System.out.println(list);
+        //  "cats and dog"
+        //  "cat sand dog"
 
 
         s = "pineapplepenapple";
