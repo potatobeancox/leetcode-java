42b94cd42288c3f06bdbb06fddc65f817731dc47
liuzhao11 <liuzhao11@sankuai.com>
2021-12-12 20:48:01 +0800

    2021.12.11 743. 网络延迟时间 2023.8.14

--- src/main/java/com/potato/study/leetcodecn/p00743/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00743/t001/Solution.java
@@ -2,6 +2,8 @@ package com.potato.study.leetcodecn.p00743.t001;
 
 import org.junit.Assert;
 
+import java.util.Arrays;
+
 /**
  * 743. 网络延迟时间
  *
@@ -47,6 +49,68 @@ import org.junit.Assert;
 public class Solution {
 
     public int networkDelayTime(int[][] times, int n, int k) {
-        return -1;
+        // 将 times 转换成 邻接矩阵 表示形式
+        int[][] graph = new int[n+1][n+1];
+        // -1 代表不可达
+        for (int[] g : graph) {
+            Arrays.fill(g, -1);
+        }
+        for (int i = 0; i < times.length; i++) {
+            int ii = times[i][0];
+            int jj = times[i][1];
+            int value = times[i][2];
+            graph[ii][jj] = value;
+        }
+        boolean[] visit = new boolean[n+1];
+        int[] dis = new int[n+1];
+        Arrays.fill(dis, Integer.MAX_VALUE - 1);
+        dis[k] = 0;
+        // 控制遍历 n 次 每次先找到 未访问过的最小的点开始进行 以这个点作为开始点 判断 是够有更小的可能 有的话 更新 （枚举 邻接点）
+        for (int i = 0; i < n; i++) {
+            // 每次先找到 未访问过的最小的点开始进行
+            int from = -1;
+            for (int j = 1; j <= n; j++) {
+                if (visit[j]) {
+                    continue;
+                }
+                if (from == - 1 || dis[from] > dis[j]) {
+                    from = j;
+                }
+            }
+            // 遍历其他点
+            for (int to = 1; to <= n; to++) {
+                if (graph[from][to] == -1) {
+                    continue;
+                }
+                if (dis[to] > dis[from] + graph[from][to]) {
+                    dis[to] = dis[from] + graph[from][to];
+                }
+            }
+            visit[from] = true;
+        }
+        // 遍历 dis 找到最大的点
+        int max = dis[1];
+        for (int i = 1; i < dis.length; i++) {
+            max = Math.max(dis[i], max);
+        }
+        return max == Integer.MAX_VALUE - 1 ? -1 : max;
     }
+
+    public static void main(String[] args) {
+        int[][] times = new int[][] {
+                {2,1,1},
+                {2,3,1},
+                {3,4,1}
+        };
+        int n = 4;
+        int k = 2;
+        Solution solution = new Solution();
+        int i = solution.networkDelayTime(times, n, k);
+        System.out.println(i);
+        Assert.assertEquals(2, i);
+
+
+    }
+
+
 }
