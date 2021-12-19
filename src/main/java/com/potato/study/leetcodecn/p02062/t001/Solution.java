3a13597a967a20816df936df6883c7f62bd76510
liuzhao11 <liuzhao11@sankuai.com>
2021-12-19 14:28:38 +0800

    2021.12.19 498. 对角线遍历 2023.8.23

---
+++ src/main/java/com/potato/study/leetcodecn/p02062/t001/Solution.java
@@ -0,0 +1,63 @@
+package com.potato.study.leetcodecn.p02062.t001;
+
+/**
+ * 2062. 统计字符串中的元音子字符串
+ *
+ * 子字符串 是字符串中的一个连续（非空）的字符序列。
+
+ 元音子字符串 是 仅 由元音（'a'、'e'、'i'、'o' 和 'u'）组成的一个子字符串，且必须包含 全部五种 元音。
+
+ 给你一个字符串 word ，统计并返回 word 中 元音子字符串的数目 。
+
+  
+
+ 示例 1：
+
+ 输入：word = "aeiouu"
+ 输出：2
+ 解释：下面列出 word 中的元音子字符串（斜体加粗部分）：
+ - "aeiouu"
+ - "aeiouu"
+ 示例 2：
+
+ 输入：word = "unicornarihan"
+ 输出：0
+ 解释：word 中不含 5 种元音，所以也不会存在元音子字符串。
+ 示例 3：
+
+ 输入：word = "cuaieuouac"
+ 输出：7
+ 解释：下面列出 word 中的元音子字符串（斜体加粗部分）：
+ - "cuaieuouac"
+ - "cuaieuouac"
+ - "cuaieuouac"
+ - "cuaieuouac"
+ - "cuaieuouac"
+ - "cuaieuouac"
+ - "cuaieuouac"
+ 示例 4：
+
+ 输入：word = "bbaeixoubb"
+ 输出：0
+ 解释：所有包含全部五种元音的子字符串都含有辅音，所以不存在元音子字符串。
+  
+
+ 提示：
+
+ 1 <= word.length <= 100
+ word 仅由小写英文字母组成
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/count-vowel-substrings-of-a-string
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    public int countVowelSubstrings(String word) {
+
+        return -1;
+    }
+
+
+}
