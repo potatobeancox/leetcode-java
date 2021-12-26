9a6a1d94ec35c6124d09498de1fc8c18f493f82a
liuzhao11 <liuzhao11@sankuai.com>
2021-12-26 23:16:34 +0800

    2021.12.26 2104. 子数组范围和 2023.9.6

---
+++ src/main/java/com/potato/study/leetcodecn/p02104/t001/Solution.java
@@ -0,0 +1,87 @@
+package com.potato.study.leetcodecn.p02104.t001;
+
+import java.util.Map;
+
+/**
+ * 2104. 子数组范围和
+ *
+ * 给你一个整数数组 nums 。nums 中，子数组的 范围 是子数组中最大元素和最小元素的差值。
+
+ 返回 nums 中 所有 子数组范围的 和 。
+
+ 子数组是数组中一个连续 非空 的元素序列。
+
+  
+
+ 示例 1：
+
+ 输入：nums = [1,2,3]
+ 输出：4
+ 解释：nums 的 6 个子数组如下所示：
+ [1]，范围 = 最大 - 最小 = 1 - 1 = 0
+ [2]，范围 = 2 - 2 = 0
+ [3]，范围 = 3 - 3 = 0
+ [1,2]，范围 = 2 - 1 = 1
+ [2,3]，范围 = 3 - 2 = 1
+ [1,2,3]，范围 = 3 - 1 = 2
+ 所有范围的和是 0 + 0 + 0 + 1 + 1 + 2 = 4
+ 示例 2：
+
+ 输入：nums = [1,3,3]
+ 输出：4
+ 解释：nums 的 6 个子数组如下所示：
+ [1]，范围 = 最大 - 最小 = 1 - 1 = 0
+ [3]，范围 = 3 - 3 = 0
+ [3]，范围 = 3 - 3 = 0
+ [1,3]，范围 = 3 - 1 = 2
+ [3,3]，范围 = 3 - 3 = 0
+ [1,3,3]，范围 = 3 - 1 = 2
+ 所有范围的和是 0 + 0 + 0 + 2 + 0 + 2 = 4
+ 示例 3：
+
+ 输入：nums = [4,-2,-3,4,1]
+ 输出：59
+ 解释：nums 中所有子数组范围的和是 59
+  
+
+ 提示：
+
+ 1 <= nums.length <= 1000
+ -109 <= nums[i] <= 109
+  
+
+ 进阶：你可以设计一种时间复杂度为 O(n) 的解决方案吗？
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/sum-of-subarray-ranges
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    public long subArrayRanges(int[] nums) {
+        int[][] dpMax = new int[nums.length][nums.length];
+        int[][] dpMin = new int[nums.length][nums.length];
+
+
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                if (i == j) {
+                    dpMax[i][j] = nums[i];
+                    dpMin[i][j] = nums[i];
+                    continue;
+                }
+                dpMax[i][j] = Math.max(dpMax[i][j-1], nums[j]);
+                dpMin[i][j] = Math.min(dpMin[i][j-1], nums[j]);
+            }
+        }
+
+        long sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i; j < nums.length; j++) {
+                sum += dpMax[i][j] - dpMin[i][j];
+            }
+        }
+        return sum;
+    }
+}
