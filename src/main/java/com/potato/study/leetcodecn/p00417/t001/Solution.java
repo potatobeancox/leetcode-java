1f669d7127ffe2288ebf1e757816c370d38fcd44
liuzhao11 <liuzhao11@sankuai.com>
2022-01-01 10:09:41 +0800

    2021.12.26 2058. 找出临界点之间的最小和最大距离 2023.9.7

--- src/main/java/com/potato/study/leetcodecn/p00417/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00417/t001/Solution.java
@@ -1,7 +1,9 @@
 package com.potato.study.leetcodecn.p00417.t001;
 
+import java.util.ArrayList;
 import java.util.List;
 
+import com.potato.study.leetcode.util.LeetcodeInputUtils;
 import org.junit.Assert;
 
 /**
@@ -49,51 +51,109 @@ public class Solution {
 
 
 
-
-
-
-
-
-
-
-
-
     // 417
     public List<List<Integer>> pacificAtlantic(int[][] heights) {
         // dp 从 第一个开始 往右下
-        boolean[][] dp1 = new boolean[heights.length][heights[0].length];
+        boolean[][] visit1 = new boolean[heights.length][heights[0].length];
+        boolean[][] result1 = new boolean[heights.length][heights[0].length];
+
+        boolean[][] visit2 = new boolean[heights.length][heights[0].length];
+        boolean[][] result2 = new boolean[heights.length][heights[0].length];
+        // 第一行和最后一行
+        for (int i = 0; i < heights[0].length; i++) {
+            visit1[0][i] = true;
+            result1[0][i] = true;
+
+            visit2[heights.length-1][i] = true;
+            result2[heights.length-1][i] = true;
+        }
+        // 第一列和最后一列
+        for (int i = 0; i < heights.length; i++) {
+            visit1[i][0] = true;
+            result1[i][0] = true;
+
+            visit2[i][heights[0].length-1] = true;
+            result2[i][heights[0].length-1] = true;
+        }
+
         for (int i = 0; i < heights.length; i++) {
             for (int j = 0; j < heights[0].length; j++) {
-                if (i == 0 || j == 0) {
-                    dp1[i][j] = true;
-                    continue;
-                }
-                if (heights[i][j] >= heights[i-1][j]) {
-                    dp1[i][j] |= dp1[i-1][j];
-                }
-                if (heights[i][j] >= heights[i][j-1]) {
-                    dp1[i][j] |= dp1[i][j-1];
-                }
+                dfs(heights, visit1, result1, i, j);
+                dfs(heights, visit2, result2, heights.length - 1 - i, heights[0].length - 1 - j);
             }
         }
 
-        boolean[][] dp2 = new boolean[heights.length][heights[0].length];
-        for (int i = heights.length - 1; i >= 0; i--) {
-            for (int j = heights[0].length - 1; j < heights[0].length; j++) {
-                if (i == 0 || j == 0) {
-                    dp2[i][j] = true;
-                    continue;
-                }
-                if (heights[i][j] >= heights[i+1][j]) {
-                    dp2[i][j] |= dp2[i+1][j];
-                }
-                if (heights[i][j] >= heights[i][j+1]) {
-                    dp2[i][j] |= dp2[i][j+1];
+
+
+        List<List<Integer>> resultList = new ArrayList<>();
+        for (int i = 0; i < heights.length; i++) {
+            for (int j = 0; j < heights[0].length; j++) {
+                if (result1[i][j] && result2[i][j]) {
+
+                    List<Integer> list = new ArrayList<>();
+                    list.add(i);
+                    list.add(j);
+
+                    resultList.add(list);
                 }
             }
         }
+        return resultList;
+    }
 
-        // dp 从最后一个位置开始 往坐上
-        return null;
+
+
+    private int[][] direction = new int[][] {
+            {1, 0},
+            {-1, 0},
+            {0, 1},
+            {0, -1}
+    };
+    /**
+     *
+     * @param heights 结果
+     * @param visit
+     * @param result
+     */
+    private void dfs(int[][] heights, boolean[][] visit, boolean[][] result, int i, int j) {
+        if (visit[i][j]) {
+            return;
+        }
+        visit[i][j] = true;
+        // 四个方向比较结果
+        for (int k = 0; k < direction.length; k++) {
+            int di = i + direction[k][0];
+            int dj = j + direction[k][1];
+            // 坐标有效性
+            if (di < 0 || di >= heights.length
+                    || dj < 0 || dj >= heights[0].length) {
+                continue;
+            }
+            // 水不能往高处流
+            if (heights[i][j] < heights[di][dj]) {
+                continue;
+            }
+            dfs(heights, visit, result, di, dj);
+            result[i][j] |= result[di][dj];
+            if (result[i][j]) {
+                break;
+            }
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String input = "[[1,2,3],[8,9,4],[7,6,5]]";
+        int[][] arr = LeetcodeInputUtils.inputString2IntArrayTwoDimensional(input);
+        List<List<Integer>> lists = solution.pacificAtlantic(arr);
+        System.out.println(lists);
+
+
+        input = "[[11,3,2,4,14,6,13,18,1,4,12,2,4,1,16],[5,11,18,0,15,14,6,17,2,17,19,15,12,3,14],[10,2,5,13,11,11,13,19,11,17,14,18,14,3,11],[14,2,10,7,5,11,6,11,15,11,6,11,12,3,11],[13,1,16,15,8,2,16,10,9,9,10,14,7,15,13],[17,12,4,17,16,5,0,4,10,15,15,15,14,5,18],[9,13,18,4,14,6,7,8,5,5,6,16,13,7,2],[19,9,16,19,16,6,1,11,7,2,12,10,9,18,19],[19,5,19,10,7,18,6,10,7,12,14,8,4,11,16],[13,3,18,9,16,12,1,0,1,14,2,6,1,16,6],[14,1,12,16,7,15,9,19,14,4,16,6,11,15,7],[6,15,19,13,3,2,13,7,19,11,13,16,0,16,16],[1,5,9,7,12,9,2,18,6,12,1,8,1,10,19],[10,11,10,11,3,5,12,0,0,8,15,7,5,13,19],[8,1,17,18,3,6,8,15,0,9,8,8,12,5,18],[8,3,6,12,18,15,10,10,12,19,16,7,17,17,1],[12,13,6,4,12,18,18,9,4,9,13,11,5,3,14],[8,4,12,11,2,2,10,3,11,17,14,2,17,4,7],[8,0,14,0,13,17,11,0,16,13,15,17,4,8,3],[18,15,8,11,18,3,10,18,3,3,15,9,11,15,15]]";
+        arr = LeetcodeInputUtils.inputString2IntArrayTwoDimensional(input);
+        lists = solution.pacificAtlantic(arr);
+        System.out.println(lists);
     }
 }
