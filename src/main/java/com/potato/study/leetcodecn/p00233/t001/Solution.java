7729b8c62188ccfd21bc50426c0c10ff1157057b
liuzhao11 <liuzhao11@sankuai.com>
2022-01-02 21:55:28 +0800

    2022.1.2 967. 连续差相同的数字 2023.9.25

--- src/main/java/com/potato/study/leetcodecn/p00233/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00233/t001/Solution.java
@@ -1,5 +1,7 @@
 package com.potato.study.leetcodecn.p00233.t001;
 
+import org.junit.Assert;
+
 import java.util.Stack;
 
 /**
@@ -30,7 +32,42 @@ import java.util.Stack;
  */
 public class Solution {
     public int countDigitOne(int n) {
+        // 从n的个位开始计数，然后一次判定有多少个1 求和
+        int count = 0;
+        int bit = 10;
+        while (n * 10 >= bit) {
+            count += (n / bit);
+            int remid = n % bit;
+            int max = bit / 10 * 2;
+            int min = bit / 10;
+            if (remid >= max) {
+                count += min;
+            } else if (remid >= min) {
+                count += (remid - min + 1);
+            }
+            bit *= 10; // 100 1000
+        }
+        return count;
+    }
 
-        return -1;
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int i = solution.countDigitOne(13);
+        System.out.println(i);
+        Assert.assertEquals(6, i);
+
+
+        i = solution.countDigitOne(0);
+        System.out.println(i);
+        Assert.assertEquals(0, i);
+
+        i = solution.countDigitOne(1);
+        System.out.println(i);
+        Assert.assertEquals(1, i);
+
+
+        i = solution.countDigitOne(100);
+        System.out.println(i);
+        Assert.assertEquals(21, i);
     }
 }
