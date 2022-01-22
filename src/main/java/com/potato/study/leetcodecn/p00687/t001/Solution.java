51621ea0d8835636b4c8d45ced48694a88eb2ce8
liuzhao11 <liuzhao11@sankuai.com>
2022-01-22 09:54:20 +0800

    2022.1.22 687. 最长同值路径 2023.11.11

--- src/main/java/com/potato/study/leetcodecn/p00687/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00687/t001/Solution.java
@@ -50,9 +50,78 @@ import com.potato.study.leetcode.domain.TreeNode;
  */
 public class Solution {
 
-    public int longestUnivaluePath(TreeNode root) {
+    private int maxLength;
 
-        return -1;
+    public int longestUnivaluePath(TreeNode root) {
+        this.maxLength = 0;
+        // 递归 函数作用 找到当前点为根的 两个相同点的最长路径
+        if (null == root) {
+            return 0;
+        }
+        longestUnivaluePathEach(root);
+        return maxLength - 1;
     }
+
+
+    private int longestUnivaluePathEach(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int len = 1;
+        int leftLen = 0;
+        int sameLen = 1;
+        if (root.left != null) {
+            leftLen = longestUnivaluePathEach(root.left);
+            if (root.val == root.left.val) {
+                len = Math.max(len, leftLen + 1);
+                sameLen += leftLen;
+            }
+        }
+        int rightLen = 0;
+        if (root.right != null) {
+            rightLen = longestUnivaluePathEach(root.right);
+            if (root.val == root.right.val) {
+                len = Math.max(len, rightLen + 1);
+                sameLen += rightLen;
+            }
+        }
+        this.maxLength = Math.max(maxLength, sameLen);
+        return len;
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        TreeNode root = new TreeNode(1);
+//        root.left = new TreeNode(4);
+        root.right = new TreeNode(1);
+        root.right.left = new TreeNode(1);
+        root.right.right = new TreeNode(1);
+
+        root.right.left.left = new TreeNode(1);
+        root.right.left.right = new TreeNode(1);
+
+        root.right.right.left = new TreeNode(1);
+//        root.right.right = new TreeNode(5);
+        int i = solution.longestUnivaluePath(root);
+        System.out.println(i);
+        Assert.assertEquals(4, i);
+
+
+
+        root = new TreeNode(5);
+        root.left = new TreeNode(4);
+        root.right = new TreeNode(5);
+        root.left.left = new TreeNode(1);
+        root.left.right = new TreeNode(1);
+
+        root.right.right = new TreeNode(5);
+//        root.right.right = new TreeNode(5);
+        i = solution.longestUnivaluePath(root);
+        System.out.println(i);
+        Assert.assertEquals(2, i);
+    }
+
+
 
 }
