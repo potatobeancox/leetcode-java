3440b68588a8ab3ef8bb63b36d205239c095cfbb
liuzhao11 <liuzhao11@sankuai.com>
2022-02-10 00:10:01 +0800

    2022.2.10 1775. 通过最少操作次数使数组的和相等 2024.1.3

--- src/main/java/com/potato/study/leetcodecn/p01775/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p01775/t001/Solution.java
@@ -51,43 +51,62 @@ import java.lang.ref.SoftReference;
 public class Solution {
 
 
+    /**
+     * 看方法2
+     * https://leetcode-cn.com/problems/equal-sum-arrays-with-minimum-number-of-operations/solution/tan-xin-xie-yi-xia-zi-ji-kan-qi-ta-ti-ji-ehyj/
+     * @param nums1
+     * @param nums2
+     * @return
+     */
     public int minOperations(int[] nums1, int[] nums2) {
-        // 统计 1-6 出现次数 两个数组 并计算 sum1和 sum2
-        int[] count1 = new int[7];
-        int[] count2 = new int[7];
+        // 统计两个 数组的和 我们领 nums2的 一定比nums1的大
+        int[] count = new int[6];
         int sum1 = 0;
         int sum2 = 0;
         for (int i = 0; i < nums1.length; i++) {
             sum1 += nums1[i];
-            count1[nums1[i]]++;
         }
         for (int i = 0; i < nums2.length; i++) {
             sum2 += nums2[i];
-            count2[nums2[i]]++;
 
         }
-        // 计算差值 从 1-6 个数遍历 每次计算 当前是否可以满足 差值达到了 改修改的大小，达到了计数 没达到返回
-        int diff = Math.abs(sum1 - sum2);
-        int step = 0;
-        for (int i = 1; i <= 6; i++) {
-            while (count1[i] > 0 || count2[i] > 0) {
-                if (diff >= (6 - i)) {
-                    diff -= (6-i);
-                    if (count1[i] > 0) {
-                        count1[i]--;
-                    } else {
-                        count2[i]--;
-                    }
-                    step++;
-                } else {
-                    return step + 1;
-                }
+        // 需要进行交换
+        if (sum1 > sum2) {
+            int[] tmp = nums1;
+            nums1 = nums2;
+            nums2 = tmp;
+
+            int tmpNum = sum1;
+            sum1 = sum2;
+            sum2 = tmpNum;
+        }
+        // num1数字扩大可以减小diff
+        for (int i = 0; i < nums1.length; i++) {
+            count[6 - nums1[i]]++;
+        }
+        // num2数字减小可以减小diff
+        for (int i = 0; i < nums2.length; i++) {
+            count[nums2[i] - 1]++;
+        }
+        int diff = sum2 - sum1;
+        // 那么计算出来的 两边的diff 那么僵diff 变小的方式 是 nums1 变大和 nums2变小，那么就看 能变小和变大的数量有多少
+        int opCount = 0;
+        for (int i = 5; i >= 0; i--) {
+            if (diff <= 0) {
+                break;
+            }
+            while (count[i] > 0 && diff > 0) {
+                diff -= i;
+                opCount++;
+                count[i]--;
             }
         }
-        if (diff != 0) {
+
+        // 利用循环遍历 数量 对 diff 进行修改 直到0
+        if (diff > 0) {
             return -1;
         }
-        return step;
+        return opCount;
     }
 
     public static void main(String[] args) {
