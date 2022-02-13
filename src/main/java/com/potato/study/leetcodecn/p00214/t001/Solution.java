869f0a8c657b25c8532018b83c03df590e30e335
liuzhao11 <liuzhao11@sankuai.com>
2022-02-13 16:44:03 +0800

    2022.2.13 214. 最短回文串 2024.1.13

--- src/main/java/com/potato/study/leetcodecn/p00214/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00214/t001/Solution.java
@@ -33,19 +33,55 @@ public class Solution {
 
 
     /**
-     * 底层kmp
-     * 然后发现 kmp 忘干净了 唉
+     * https://leetcode-cn.com/problems/shortest-palindrome/solution/java-kmp-by-lyl-36-zrfx/
      * @param s
      * @return
      */
     public String shortestPalindrome(String s) {
-        // 添加前缀，将其变成 回文串，找到最短的回文串
+        // 将s 翻转 拼到s 之后
+        StringBuilder builder = new StringBuilder(s);
+        String target = s + "#" + builder.reverse().toString();
+        // 使用next 数组 表示 如果当前字符串匹配不上的话 下一个字符从哪个字符开始匹配
+        int[] next = new int[target.length()];
+        // 如果开始位置就匹配不上 那么从 -1 + 1开始匹配
+        next[0] = -1;
+        // next 存的是 这个位置之前匹配上的位置
+        int j = -1;
+        for (int i = 1; i < target.length(); i++) {
+            // 找到应该匹配的位置
+            while (j != -1 &&
+                    target.charAt(i) != target.charAt(j+1)) {
+                j = next[j];
+            }
+            // 判断能不能匹配上 匹配上的话 往后移动一下记录
+            if (target.charAt(i) == target.charAt(j+1)) {
+                j += 1;
+            }
+            next[i] = j;
+        }
+        // 找到最后一个字符 匹配不上 从哪个开始匹配的位置  也就是 s 之前是可以公用的 往后生成 字符串
+        int pos = next[target.length() - 1] + 1;
+        // 拼接到s 上
+        if (pos == -1) {
+            return builder.reverse().toString() + s;
+        }
+        String substring = s.substring(pos);
+        return new StringBuilder(substring).reverse().toString() + s;
+    }
 
 
-        // 最长回文串 是将 s.sub 1 翻转拼在前边
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String str = "aacecaaa";
+        String s = solution.shortestPalindrome(str);
+        System.out.println(s);
+        Assert.assertEquals("aaacecaaa", s);
 
-        
-        return null;
+
+        str = "abcd";
+        s = solution.shortestPalindrome(str);
+        System.out.println(s);
+        Assert.assertEquals("dcbabcd", s);
     }
 
 
