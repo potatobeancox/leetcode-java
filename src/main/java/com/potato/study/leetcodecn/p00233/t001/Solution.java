8bd4da412f7a3f2306cd3698c50719ce0fa4fb00
liuzhao11 <liuzhao11@sankuai.com>
2022-02-16 00:00:29 +0800

    2022.2.15 233. 数字 1 的个数 2024.1.15

--- src/main/java/com/potato/study/leetcodecn/p00233/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00233/t001/Solution.java
@@ -31,23 +31,41 @@ import java.util.Stack;
  *
  */
 public class Solution {
+
+
+    /**
+     * https://leetcode-cn.com/problems/number-of-digit-one/solution/gong-shui-san-xie-jiang-shu-wei-dp-wen-t-c9oi/
+     * @param n
+     * @return
+     */
     public int countDigitOne(int n) {
-        // 从n的个位开始计数，然后一次判定有多少个1 求和
-        int count = 0;
-        int bit = 10;
-        while (n * 10 >= bit) {
-            count += (n / bit);
-            int remid = n % bit;
-            int max = bit / 10 * 2;
-            int min = bit / 10;
-            if (remid >= max) {
-                count += min;
-            } else if (remid >= min) {
-                count += (remid - min + 1);
-            }
-            bit *= 10; // 100 1000
+        char[] chars = String.valueOf(n).toCharArray();
+        // 预处理出来一个当前位置前面 位置的 数字和后面位置的数字
+        int[] prefix = new int[chars.length];
+        for (int i = 1; i < chars.length; i++) {
+            prefix[i] = 10 * prefix[i-1] + (chars[i-1] - '0');
         }
-        return count;
+        int[] suffix = new int[chars.length];
+        for (int i = chars.length - 2; i >= 0; i--) {
+            suffix[i] = suffix[i+1] + (chars[i+1] - '0') * (int) Math.pow(10, chars.length - 2 - i);
+        }
+        // 遍历 n的每个位置
+        int totalCount = 0;
+        for (int i = 0; i < chars.length; i++) {
+            int p = prefix[i];
+            int s = suffix[i];
+            // abc c的时候 i = 2 len = 3
+            int bit = (int) Math.pow(10, chars.length - i - 1);
+            totalCount += p * bit;
+            if (chars[i] == '1') {
+                // 如果当前位置数字小于1 那么只有 prefix * 100 数字 等于 1 + 后缀的数字
+                totalCount += (s + 1);
+            } else if (chars[i] > '1') {
+                // 如果当前位置 数字 大于1 说明 prefix * 100  + 1 * 100 类似 假设i后面有两个数字
+                totalCount += bit;
+            }
+        }
+        return totalCount;
     }
 
     public static void main(String[] args) {
