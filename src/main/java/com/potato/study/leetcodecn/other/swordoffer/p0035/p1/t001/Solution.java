2b5620b93543028f662de77a42d22d0a359dab26
liuzhao03 <liuzhao03@kuaishou.com>
2022-02-19 18:04:37 +0800

    2022.2.19 剑指 Offer 35. 复杂链表的复制 2024.1.22

---
+++ src/main/java/com/potato/study/leetcodecn/other/swordoffer/p0035/p1/t001/Solution.java
@@ -0,0 +1,92 @@
+package com.potato.study.leetcodecn.other.swordoffer.p0035.p1.t001;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.sun.jmx.snmp.SnmpNull;
+
+/**
+ * 剑指 Offer 35. 复杂链表的复制
+ *
+ * 请实现 copyRandomList 函数，复制一个复杂链表。在复杂链表中，每个节点除了有一个 next 指针指向下一个节点，还有一个 random 指针指向链表中的任意节点或者 null。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ *
+ * 输入：head = [[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 输出：[[7,null],[13,0],[11,4],[10,2],[1,0]]
+ * 示例 2：
+ *
+ *
+ *
+ * 输入：head = [[1,1],[2,1]]
+ * 输出：[[1,1],[2,1]]
+ * 示例 3：
+ *
+ *
+ *
+ * 输入：head = [[3,null],[3,0],[3,null]]
+ * 输出：[[3,null],[3,0],[3,null]]
+ * 示例 4：
+ *
+ * 输入：head = []
+ * 输出：[]
+ * 解释：给定的链表为空（空指针），因此返回 null。
+ *  
+ *
+ * 提示：
+ *
+ * -10000 <= Node.val <= 10000
+ * Node.random 为空（null）或指向链表中的节点。
+ * 节点数目不超过 1000 。
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/fu-za-lian-biao-de-fu-zhi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    class Node {
+        int val;
+        Node next;
+        Node random;
+
+        public Node(int val) {
+            this.val = val;
+            this.next = null;
+            this.random = null;
+        }
+    }
+
+
+    private Map<Node, Node> map = new HashMap<>();
+
+
+    public Node copyRandomList(Node head) {
+        if (head == null) {
+            return null;
+        }
+        Node node = map.get(head);
+        if (node == null) {
+            // 创建
+            node = new Node(head.val);
+            map.put(head, node);
+            if (node.next == null) {
+                node.next = copyRandomList(head.next);
+            }
+            if (node.random == null) {
+                node.random = copyRandomList(head.random);
+            }
+        }
+        return node;
+    }
+
+
+
+}
