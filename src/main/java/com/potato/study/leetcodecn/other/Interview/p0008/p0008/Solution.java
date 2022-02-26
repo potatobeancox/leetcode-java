c43ccc826f29d477068bd48b1bfdcb82452c892b
liuzhao03 <liuzhao03@kuaishou.com>
2022-02-26 21:30:11 +0800

    2022.2.23 987. 二叉树的垂序遍历 2024.2.2

---
+++ src/main/java/com/potato/study/leetcodecn/other/Interview/p0008/p0008/Solution.java
@@ -0,0 +1,46 @@
+package com.potato.study.leetcodecn.other.Interview.p0008.p0008;
+
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 面试题 08.08. 有重复字符串的排列组合
+ *
+ * 有重复字符串的排列组合。编写一种方法，计算某字符串的所有排列组合。
+ *
+ * 示例1:
+ *
+ *  输入：S = "qqe"
+ *  输出：["eqq","qeq","qqe"]
+ * 示例2:
+ *
+ *  输入：S = "ab"
+ *  输出：["ab", "ba"]
+ * 提示:
+ *
+ * 字符都是英文字母。
+ * 字符串长度在[1, 9]之间。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/permutation-ii-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+
+    public String[] permutation(String s) {
+        List<String> result = new ArrayList<>();
+
+        String[] res = new String[result.size()];
+        for (int i = 0; i < result.size(); i++) {
+
+        }
+        return res;
+    }
+
+}
