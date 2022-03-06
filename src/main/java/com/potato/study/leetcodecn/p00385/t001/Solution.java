11496aa9902336bf569eedcdaaea291aa82910b2
liuzhao11 <liuzhao11@sankuai.com>
2022-03-06 23:02:41 +0800

    2022.3.6 310. 最小高度树 2024.3.3

--- src/main/java/com/potato/study/leetcodecn/p00385/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00385/t001/Solution.java
@@ -1,7 +1,6 @@
 package com.potato.study.leetcodecn.p00385.t001;
 
 import java.util.List;
-import java.util.Random;
 
 /**
  * 385. 迷你语法分析器
@@ -41,49 +40,92 @@ import java.util.Random;
  */
 public class Solution {
 
+    /**
+     * 一看就是一个递归结构
+     * https://leetcode-cn.com/problems/mini-parser/solution/385-mi-ni-yu-fa-fen-xi-qi-dfs-by-nortondark/
+     * @param s
+     * @return
+     */
     public NestedInteger deserialize(String s) {
-        // 开始位置 【 和 结束位置 】
-        String substring = s.substring(1, s.length() - 1);
-        // 找到 ，
-        int index = 0;
-        while (index < substring.length() && substring.charAt(index) != ',') {
-            index++;
+        if (s == null || s.length() == 0) {
+            return new NestedInteger();
         }
-        String numStr = substring.substring(0, index);
-        int num = Integer.parseInt(numStr);
-        NestedInteger nestedInteger = new NestedInteger(num);
-        // 递归生成 NestedInteger 放入
-        return null;
+        // 如果当前就是一个数字 直接 返回
+        if (s.charAt(0) != '[') {
+            return new NestedInteger(Integer.parseInt(s));
+        }
+        // 当前就一个空 【】
+        if (s.length() == 2) {
+            return new NestedInteger();
+        }
+        // 对s 开始遍历 每个位置 记录 当前开始位置 和 当前位置 如果是数字 直接 continue ，如果 是 【】 对 count 进行修改
+        NestedInteger current = new NestedInteger();
+        int start = 1;
+        // [ 数量
+        int count = 0;
+        for (int i = 1; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            // 先判断 是否可以递归
+            if (count == 0 && (ch == ',' || i == s.length() - 1)) {
+                String substring = s.substring(start, i);
+                current.add(deserialize(substring));
+                start = i + 1;
+            }
+            if ('[' == ch) {
+                count++;
+            } else if (']' == ch) {
+                count--;
+            }
+        }
+
+        // 如果遇到 ， 或者 遇到末尾 递归对子串进行生成 必须要保证 count == 0 才能递归
+        return current;
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "324";
+        NestedInteger deserialize = solution.deserialize(s);
+
+        s = "[123,[456,[789]]]";
+        deserialize = solution.deserialize(s);
     }
 }
 
 
 class NestedInteger {
 
-    public NestedInteger() {
+    private int value;
+    private boolean isInteger;
 
+    private NestedInteger child;
+
+    public NestedInteger() {
+        this.isInteger = false;
     }
 
     public NestedInteger(int value) {
-
+        this.value = value;
+        this.isInteger = true;
     }
     // @return true if this NestedInteger holds a single integer, rather than a nested list.
     public boolean isInteger() {
-        return false;
+        return isInteger;
     }
     // @return the single integer that this NestedInteger holds, if it holds a single integer
     // Return null if this NestedInteger holds a nested list
     public Integer getInteger() {
-        return -1;
+        return this.value;
     }
 
     // Set this NestedInteger to hold a single integer.
     public void setInteger(int value) {
-
+        this.value = value;
     }
     // Set this NestedInteger to hold a nested list and adds a nested integer to it.
     public void add(NestedInteger ni) {
-
+        this.child = ni;
     }
     // @return the nested list that this NestedInteger holds, if it holds a nested list
     // Return empty list if this NestedInteger holds a single integer
