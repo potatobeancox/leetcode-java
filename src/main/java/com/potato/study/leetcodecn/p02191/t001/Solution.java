c7dfa0e62088978f223796c9800af590f8f608d0
liuzhao11 <liuzhao11@sankuai.com>
2022-03-31 23:03:22 +0800

    2022.3.28 2191. 将杂乱无章的数字排序 2024.4.27

--- src/main/java/com/potato/study/leetcodecn/p02191/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p02191/t001/Solution.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.PriorityQueue;
 
 /**
  * 2191. 将杂乱无章的数字排序
@@ -56,9 +57,33 @@ import java.util.Map;
 public class Solution {
 
 
+    // 2191
     public int[] sortJumbled(int[] mapping, int[] nums) {
-
-        return null;
+        // 0-原来值. 1-实际值 , 2- index
+        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>((a1, a2) -> {
+            int compare = Integer.compare(a1[1], a2[1]);
+            if (compare != 0) {
+                return compare;
+            }
+            return Integer.compare(a1[2], a2[2]);
+        });
+        // 生成数组
+        for (int i = 0; i < nums.length; i++) {
+            char[] chars = String.valueOf(nums[i]).toCharArray();
+            StringBuilder builder = new StringBuilder();
+            for (int j = 0; j < chars.length; j++) {
+                builder.append(mapping[chars[j] - '0']);
+            }
+            int target = Integer.parseInt(builder.toString());
+            priorityQueue.add(new int[]{nums[i], target, i});
+        }
+        int[] result = new int[nums.length];
+        int i = 0;
+        while (!priorityQueue.isEmpty()) {
+            int[] poll = priorityQueue.poll();
+            result[i++] = poll[0];
+        }
+        return result;
     }
 
 }
