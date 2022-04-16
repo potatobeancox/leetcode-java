bfd673b119d3ec7c4657f656e1d46d93e86704df
liuzhao11 <liuzhao11@sankuai.com>
2022-04-16 10:35:55 +0800

    2022.4.16 2235. 两整数相加 2024.6.6

---
+++ src/main/java/com/potato/study/leetcodecn/p02235/t001/Solution.java
@@ -0,0 +1,38 @@
+package com.potato.study.leetcodecn.p02235.t001;
+
+import com.potato.study.leetcode.domain.TreeNode;
+
+/**
+ * 2235. 两整数相加
+ *
+ * 给你两个整数 num1 和 num2，返回这两个整数的和。
+  
+
+ 示例 1：
+
+ 输入：num1 = 12, num2 = 5
+ 输出：17
+ 解释：num1 是 12，num2 是 5 ，它们的和是 12 + 5 = 17 ，因此返回 17 。
+ 示例 2：
+
+ 输入：num1 = -10, num2 = 4
+ 输出：-6
+ 解释：num1 + num2 = -6 ，因此返回 -6 。
+  
+
+ 提示：
+
+ -100 <= num1, num2 <= 100
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/add-two-integers
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
