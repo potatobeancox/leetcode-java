9de0ac0dfa63c550b6eb961fee442a2f8a89fa98
liuzhao03 <liuzhao03@kuaishou.com>
2022-04-30 13:25:00 +0800

    2022.4.29 2075. 解码斜向换位密码 2024.8.3

---
+++ src/main/java/com/potato/study/leetcodecn/p00790/t001/Solution.java
@@ -0,0 +1,47 @@
+package com.potato.study.leetcodecn.p00790.t001;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 790. 多米诺和托米诺平铺
+ *
+ * 有两种形状的瓷砖：一种是 2 x 1 的多米诺形，另一种是形如 "L" 的托米诺形。两种形状都可以旋转。
+ *
+ *
+ *
+ * 给定整数 n ，返回可以平铺 2 x n 的面板的方法的数量。返回对 109 + 7 取模 的值。
+ *
+ * 平铺指的是每个正方形都必须有瓷砖覆盖。两个平铺不同，当且仅当面板上有四个方向上的相邻单元中的两个，使得恰好有一个平铺有一个瓷砖占据两个正方形。
+ *
+ *  
+ *
+ * 示例 1:
+ *
+ *
+ *
+ * 输入: n = 3
+ * 输出: 5
+ * 解释: 五种不同的方法如上所示。
+ * 示例 2:
+ *
+ * 输入: n = 1
+ * 输出: 1
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= n <= 1000
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/domino-and-tromino-tiling
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    public int numTilings(int n) {
+
+        return -1;
+    }
+}
