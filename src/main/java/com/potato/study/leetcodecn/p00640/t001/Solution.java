16702dee28e796d9916c9211614508f35cd3c931
liuzhao03 <liuzhao03@kuaishou.com>
2022-05-01 17:38:11 +0800

    2022.5.1 640. 求解方程 2024.8.9

--- src/main/java/com/potato/study/leetcodecn/p00640/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00640/t001/Solution.java
@@ -1,7 +1,10 @@
 package com.potato.study.leetcodecn.p00640.t001;
 
 
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
 
 /**
  * 640. 求解方程
@@ -43,6 +46,7 @@ import java.util.Queue;
 public class Solution {
 
     public String solveEquation(String equation) {
+        // 按照 = 分割
         if (!equation.contains("=")) {
             return "No solution";
         }
@@ -51,14 +55,97 @@ public class Solution {
         if (split.length != 2) {
             return "No solution";
         }
-        // 记录+- 和数字 通过变量记录
-        for (int i = 0; i < 2; i++) {
-            String eq = split[i];
-            for (int j = 0; j < eq.length(); j++) {
-
+        // 分别对于左边和右边 按照 +- 拆分
+        int x = 0;
+        int num = 0;
+        // 等式左边
+        List<String> list = splitByOperator(split[0]);
+        for (String op : list) {
+            if (op.contains("x")) {
+                if ("x".equals(op)) {
+                    x += 1;
+                } else if ("+x".equals(op)) {
+                    x += 1;
+                } else if ("-x".equals(op)) {
+                    x -= 1;
+                } else {
+                    x += Integer.parseInt(op.substring(0, op.length() - 1));
+                }
+            } else {
+                num -= Integer.parseInt(op);
             }
         }
-        return null;
+        // 等式右边
+        list = splitByOperator(split[1]);
+        for (String op : list) {
+            if (op.contains("x")) {
+                if ("x".equals(op)) {
+                    x -= 1;
+                } else if ("+x".equals(op)) {
+                    x -= 1;
+                } else if ("-x".equals(op)) {
+                    x += 1;
+                } else {
+                    x -= Integer.parseInt(op.substring(0, op.length() - 1));
+                }
+            } else {
+                num += Integer.parseInt(op);
+            }
+        }
+        if (x == 0) {
+            if (num == 0) {
+                return "Infinite solutions";
+            } else {
+                return "No solution";
+            }
+        }
+        StringBuilder builder = new StringBuilder();
+        builder.append("x=");
+        builder.append(num / x);
+        return builder.toString();
+    }
+
+
+    private List<String> splitByOperator(String equation) {
+        char[] chars = equation.toCharArray();
+        int i = 0;
+        StringBuilder builder = new StringBuilder();
+        List<String> resultList = new ArrayList<>();
+        while (i < chars.length) {
+            char ch = chars[i];
+            if (ch == '+' || ch == '-') {
+                // 之前积累的字符
+                if (builder.length() > 0) {
+                    resultList.add(builder.toString());
+                }
+                builder = new StringBuilder();
+                if (ch == '-') {
+                    builder.append(ch);
+                }
+            } else {
+                builder.append(ch);
+            }
+            i++;
+        }
+        if (builder.length() != 0) {
+            resultList.add(builder.toString());
+        }
+        return resultList;
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String equation = "x+5-3+x=6+x-2";
+        String s = solution.solveEquation(equation);
+        System.out.println(s);
+        Assert.assertEquals("x=2", s);
+
+
+        equation = "-x=-1";
+        s = solution.solveEquation(equation);
+        System.out.println(s);
+        Assert.assertEquals("x=1", s);
     }
 
 }
