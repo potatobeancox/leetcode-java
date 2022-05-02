e0f7bc3c08b651004ea60aa15a646aa8272e793f
liuzhao03 <liuzhao03@kuaishou.com>
2022-05-02 13:37:18 +0800

    2022.5.1 2096. 从二叉树一个节点到另一个节点每一步的方向 2024.8.10

--- src/main/java/com/potato/study/leetcodecn/p02096/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p02096/t001/Solution.java
@@ -1,6 +1,13 @@
 package com.potato.study.leetcodecn.p02096.t001;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Assert;
+
 import com.potato.study.leetcode.domain.TreeNode;
+import com.potato.study.leetcode.util.LeetcodeInputUtils;
+import com.potato.study.leetcode.util.TreeNodeUtil;
 
 /**
  * 2096. 从二叉树一个节点到另一个节点每一步的方向
@@ -48,8 +55,119 @@ import com.potato.study.leetcode.domain.TreeNode;
  */
 public class Solution {
 
-    public String getDirections(TreeNode root, int startValue, int destValue) {
+    private List<Character> leftResult;
+    private List<Character> rightResult;
 
+    public String getDirections(TreeNode root, int startValue, int destValue) {
+        // 找到 startValue 和 destValue 的共同祖先
+        TreeNode commonRoot = getCommonRoot(root, startValue, destValue);
+        // 从共同祖先开始 dfs 找到 startValue 并生成路径
+        this.leftResult = new ArrayList<>();
+        dfsFromStart(commonRoot, startValue, new ArrayList<>());
+        // 从共同祖先开始 dfs 找到 destValue 并生成路径
+        this.rightResult = new ArrayList<>();
+        dfsFromDest(commonRoot, destValue, new ArrayList<>());
+        StringBuilder builder = new StringBuilder();
+        for (Character ch : leftResult) {
+            builder.append(ch);
+        }
+        for (Character ch : rightResult) {
+            builder.append(ch);
+        }
+        return builder.toString();
+    }
+
+    private void dfsFromStart(TreeNode commonRoot, int startValue, List<Character> temp) {
+        if (this.leftResult.size() > 0) {
+            return;
+        }
+        if (commonRoot == null) {
+            return;
+        }
+        // 当前 tmp 就是结果
+        if (commonRoot.val == startValue) {
+            this.leftResult = new ArrayList<>(temp);
+            return;
+        }
+        // 往两个孩子找
+//        List<Character> nextTemp = new ArrayList<>(temp);
+        temp.add('U');
+        dfsFromStart(commonRoot.left, startValue, temp);
+        temp.remove(temp.size() - 1);
+        temp.add('U');
+        dfsFromStart(commonRoot.right, startValue, temp);
+        temp.remove(temp.size() - 1);
+    }
+
+    private void dfsFromDest(TreeNode commonRoot, int destValue, List<Character> temp) {
+        if (this.rightResult.size() > 0) {
+            return;
+        }
+        if (commonRoot == null) {
+            return;
+        }
+        // 当前 tmp 就是结果
+        if (commonRoot.val == destValue) {
+            this.rightResult = new ArrayList<>(temp);
+            return;
+        }
+        // 往两个孩子找
+//        List<Character> nextTemp = new ArrayList<>(temp);
+        temp.add('L');
+        dfsFromDest(commonRoot.left, destValue, temp);
+        temp.remove(temp.size() - 1);
+        temp.add('R');
+        dfsFromDest(commonRoot.right, destValue, temp);
+        temp.remove(temp.size() - 1);
+    }
+
+
+
+    private TreeNode getCommonRoot(TreeNode root, int startValue, int destValue) {
+        if (root == null) {
+            return null;
+        }
+        if (root.val == startValue || root.val == destValue) {
+            return root;
+        }
+        TreeNode leftCommonRoot = getCommonRoot(root.left, startValue, destValue);
+        TreeNode rightCommonRoot = getCommonRoot(root.right, startValue, destValue);
+        // 分别在两侧找到 说明这个点就是 当前的点
+        if (leftCommonRoot != null && rightCommonRoot != null) {
+            return root;
+        }
+        if (leftCommonRoot != null) {
+            return leftCommonRoot;
+        }
+        if (rightCommonRoot != null) {
+            return rightCommonRoot;
+        }
+        //全没找到
         return null;
     }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String str = "[5,1,2,3,null,6,4]";
+        TreeNode root = TreeNodeUtil.generateTreeByArrayString(str);
+        int startValue = 3;
+        int destValue = 6;
+        String directions = solution.getDirections(root, startValue, destValue);
+        System.out.println(directions);
+        Assert.assertEquals("UURL", directions);
+
+
+        str = "[3,1,2]";
+        root = TreeNodeUtil.generateTreeByArrayString(str);
+        startValue = 2;
+        destValue = 1;
+        directions = solution.getDirections(root, startValue, destValue);
+        System.out.println(directions);
+        Assert.assertEquals("UL", directions);
+
+    }
+
+
+
+
 }
