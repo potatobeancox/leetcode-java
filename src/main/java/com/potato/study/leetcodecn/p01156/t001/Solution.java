d6beedb93fa82a01c085ce64bf34b1d80f56e421
liuzhao03 <liuzhao03@kuaishou.com>
2022-05-07 22:35:08 +0800

    2022.5.5 2246. 相邻字符不同的最长路径 2024.9.4

---
+++ src/main/java/com/potato/study/leetcodecn/p01156/t001/Solution.java
@@ -0,0 +1,52 @@
+package com.potato.study.leetcodecn.p01156.t001;
+
+
+/**
+ * 1156. 单字符重复子串的最大长度
+ *
+ * 如果字符串中的所有字符都相同，那么这个字符串是单字符重复的字符串。
+ *
+ * 给你一个字符串 text，你只能交换其中两个字符一次或者什么都不做，然后得到一些单字符重复的子串。返回其中最长的子串的长度。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：text = "ababa"
+ * 输出：3
+ * 示例 2：
+ *
+ * 输入：text = "aaabaaa"
+ * 输出：6
+ * 示例 3：
+ *
+ * 输入：text = "aaabbaaa"
+ * 输出：4
+ * 示例 4：
+ *
+ * 输入：text = "aaaaa"
+ * 输出：5
+ * 示例 5：
+ *
+ * 输入：text = "abcdef"
+ * 输出：1
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= text.length <= 20000
+ * text 仅由小写英文字母组成。
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/swap-for-longest-repeated-character-substring
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    public int maxRepOpt1(String text) {
+
+        return -1;
+    }
+}
