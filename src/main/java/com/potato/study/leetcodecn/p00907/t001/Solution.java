d11660e4dc39bc50c8ade854bf7212e899dfebd1
liuzhao11 <liuzhao11@sankuai.com>
2022-05-09 22:59:58 +0800

    2022.5.8 1353. 最多可以参加的会议数目 2024.9.24

---
+++ src/main/java/com/potato/study/leetcodecn/p00907/t001/Solution.java
@@ -0,0 +1,43 @@
+package com.potato.study.leetcodecn.p00907.t001;
+
+/**
+ * 907. 子数组的最小值之和
+ *
+ * 给定一个整数数组 arr，找到 min(b) 的总和，其中 b 的范围为 arr 的每个（连续）子数组。
+
+ 由于答案可能很大，因此 返回答案模 10^9 + 7 。
+
+  
+
+ 示例 1：
+
+ 输入：arr = [3,1,2,4]
+ 输出：17
+ 解释：
+ 子数组为 [3]，[1]，[2]，[4]，[3,1]，[1,2]，[2,4]，[3,1,2]，[1,2,4]，[3,1,2,4]。
+ 最小值为 3，1，2，4，1，1，2，1，1，1，和为 17。
+ 示例 2：
+
+ 输入：arr = [11,81,94,43,3]
+ 输出：444
+  
+
+ 提示：
+
+ 1 <= arr.length <= 3 * 104
+ 1 <= arr[i] <= 3 * 104
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode.cn/problems/sum-of-subarray-minimums
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    public int sumSubarrayMins(int[] arr) {
+
+        return -1;
+    }
+
+
+}
