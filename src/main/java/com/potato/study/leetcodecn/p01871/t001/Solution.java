b2d7f8364eea7bf2549c50c5b488572bc6a3e9cd
liuzhao03 <liuzhao03@kuaishou.com>
2022-05-23 19:49:37 +0800

    2022.5.22 1871. 跳跃游戏 VII 2024.10.14

--- src/main/java/com/potato/study/leetcodecn/p01871/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p01871/t001/Solution.java
@@ -1,5 +1,7 @@
 package com.potato.study.leetcodecn.p01871.t001;
 
+import org.junit.Assert;
+
 /**
  * 1871. 跳跃游戏 VII
  *
@@ -38,8 +40,44 @@ package com.potato.study.leetcodecn.p01871.t001;
  */
 public class Solution {
 
+    /**
+     * https://leetcode.cn/problems/jump-game-vii/solution/jian-dan-de-dong-tai-gui-hua-zhuang-tai-260xm/
+     * @param s
+     * @param minJump
+     * @param maxJump
+     * @return
+     */
     public boolean canReach(String s, int minJump, int maxJump) {
+        // 状态数组记录 当前点是否能够达到
+        boolean[] visit = new boolean[s.length()];
+        visit[0] = true;
+        // 枚举开始 位置 如果是走不到的点 continue 下个位置
+        int hasNotVisitIndex = 0;
+        for (int i = 0; i < s.length(); i++) {
+            // 这个点之前走不到
+            if (!visit[i]) {
+                continue;
+            }
+            // 最晚的位置
+            int startIndex = Math.max(minJump + i, hasNotVisitIndex);
+            int endIndex = Math.min(maxJump + i, s.length() - 1);
+            // 如果是 能够到达的点 找到 minJump 和已经访问过的最大位置 maxJump 和最后一个位置的最大位置
+            for (int j = startIndex; j <= endIndex; j++) {
+                visit[j] = (s.charAt(j) == '0');
+            }
+            hasNotVisitIndex = endIndex+1;
+        }
+        // 如果 si 是 0 说明能够到达，否则不能到达
+        return visit[s.length() - 1];
+    }
 
-        return false;
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "011010";
+        int minJump = 2;
+        int maxJump = 3;
+        boolean b = solution.canReach(s, minJump, maxJump);
+        System.out.println(b);
+        Assert.assertEquals(true, b);
     }
 }
