1e15bbde140e45c6b36450b6a993abe27d7c817d
liuzhao03 <liuzhao03@kuaishou.com>
2022-05-28 22:32:05 +0800

    2022.5.28 1546. 和为目标值且不重叠的非空子数组的最大数目 2024.10.30

--- src/main/java/com/potato/study/leetcodecn/p01156/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p01156/t001/Solution.java
@@ -1,6 +1,8 @@
 package com.potato.study.leetcodecn.p01156.t001;
 
 
+import org.junit.Assert;
+
 /**
  * 1156. 单字符重复子串的最大长度
  *
@@ -45,8 +47,66 @@ package com.potato.study.leetcodecn.p01156.t001;
  */
 public class Solution {
 
+    /**
+     * https://leetcode.cn/problems/swap-for-longest-repeated-character-substring/solution/java-tong-ji-ge-zi-mu-chu-xian-ci-shu-zai-shun-xu-/
+     * @param text
+     * @return
+     */
     public int maxRepOpt1(String text) {
-
-        return -1;
+        // 遍历 text 记录所有字母出现次数
+        int[] totalCount = new int[26];
+        char[] chars = text.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            totalCount[chars[i]-'a']++;
+        }
+        // 遍历 text 使用数组 windowCount 记录窗口内部的出现次数
+        int[] windowCount = new int[26];
+        int maxCount = 0;
+        int maxCharIndex = 0;
+        // 如果当前记录长度已经大于 最大长度了，说明当前出现次数就是 最大长度 修改最大长度 最大长度 + 1等
+        int startIndex = 0;
+        for (int i = 0; i < text.length(); i++) {
+            char ch = chars[i];
+            int charIndex = ch - 'a';
+            // 窗口内 计数
+            windowCount[charIndex]++;
+            // 如果当前窗口长度 比最大窗口 + 1还要大 说明 需要移动遍 一次
+            if (windowCount[charIndex] > maxCount) {
+                maxCount = windowCount[charIndex];
+                maxCharIndex = charIndex;
+            } else {
+                // 窗口是否需要缩小
+                if (i - startIndex + 1 > maxCount + 1) {
+                    windowCount[chars[startIndex] - 'a']--;
+                    startIndex++;
+                }
+            }
+        }
+        // 返回最大字符出现次数 和 maxCount + 1 的最小值
+        return Math.min(maxCount + 1, totalCount[maxCharIndex]);
     }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String text = "ababa";
+        int i = solution.maxRepOpt1(text);
+        System.out.println(i);
+        Assert.assertEquals(3, i);
+
+        text = "aaabaaa";
+        i = solution.maxRepOpt1(text);
+        System.out.println(i);
+        Assert.assertEquals(6, i);
+
+        text = "aaabbaaa";
+        i = solution.maxRepOpt1(text);
+        System.out.println(i);
+        Assert.assertEquals(4, i);
+
+        text = "abbccbcaaa";
+        i = solution.maxRepOpt1(text);
+        System.out.println(i);
+        Assert.assertEquals(4, i);
+    }
+
 }
