7f50fbd90c5afd9c88cb1c1e41d33a0c842b8045
liuzhao11 <liuzhao11@sankuai.com>
2022-06-04 10:40:46 +0800

    2022.6.4 剑指 Offer 51. 数组中的逆序对 2024.11.15

--- src/main/java/com/potato/study/leetcodecn/other/swordoffer/p0051/p1/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/other/swordoffer/p0051/p1/t001/Solution.java
@@ -1,5 +1,7 @@
 package com.potato.study.leetcodecn.other.swordoffer.p0051.p1.t001;
 
+import org.junit.Assert;
+
 /**
  * 剑指 Offer 51. 数组中的逆序对
  *
@@ -24,9 +26,96 @@ package com.potato.study.leetcodecn.other.swordoffer.p0051.p1.t001;
  */
 public class Solution {
 
-    public int reversePairs(int[] nums) {
 
-        return -1;
+    private int total;
+
+    public int reversePairs(int[] nums) {
+        this.total = 0;
+        if (nums.length == 0) {
+            return 0;
+        }
+        // 归并 排序 merge的时候 计算每个位置 的逆序
+        int[] temp = new int[nums.length];
+        mergeAndSort(nums, 0, nums.length - 1, temp);
+        return total;
+    }
+
+    /**
+     * 归并排序
+     * @param nums
+     * @param left
+     * @param right
+     */
+    private void mergeAndSort(int[] nums, int left, int right, int[] temp) {
+        // 终止条件 已经找打一个节点
+        if (left == right) {
+            return;
+        }
+        // 按照 mid 递归拆分
+        int mid = (left + right) / 2;
+        mergeAndSort(nums, left, mid, temp);
+        mergeAndSort(nums, mid + 1, right, temp);
+
+        // 如果mid 和 mid + 1已经有序了 剪枝
+        if (nums[mid] <= nums[mid+1]) {
+            return;
+        }
+
+        // 到这两个数组已经是有序的 进行合并
+        merge(nums, left, right, mid, temp);
+    }
+
+    /**
+     * 对数组的 left mid， mid+1， right 进行排序
+     * @param nums
+     * @param left
+     * @param right
+     * @param mid
+     */
+    private void merge(int[] nums, int left, int right, int mid, int[] temp) {
+        int i = left;
+        int j = mid + 1;
+
+        for (int k = left; k <= right; k++) {
+            temp[k] = nums[k];
+        }
+
+        for (int k = left; k <= right; k++) {
+            // 两个数组 某一个备用完了
+            if (i > mid) {
+                // 只能用j了 没有逆序
+                nums[k] = temp[j];
+                j++;
+            } else if (j > right) {
+                nums[k] = temp[i];
+                // 存在逆序
+                total += (right - (mid+1) + 1);
+                i++;
+            } else {
+                // 两个数组 都没有被用完 找小的
+                if (temp[i] <= temp[j]) {
+                    // 用了 i 也存在逆序
+                    nums[k] = temp[i];
+                    total += (j-1 - (mid+1) + 1);
+                    i++;
+                } else {
+                    // j位置 小于 i
+                    nums[k] = temp[j];
+                    j++;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr = new int[] {
+                7,5,6,4
+        };
+        int i = solution.reversePairs(arr);
+        System.out.println(i);
+        Assert.assertEquals(5, i);
+
     }
 
 }
