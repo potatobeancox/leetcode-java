a31af9efaf003775f5abd8faf93210e381a93a24
liuzhao11 <liuzhao11@sankuai.com>
2022-06-05 17:26:45 +0800

    2022.6.5 面试题 08.08. 有重复字符串的排列组合 2024.11.19

--- src/main/java/com/potato/study/leetcodecn/other/Interview/p0008/p0008/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/other/Interview/p0008/p0008/Solution.java
@@ -34,13 +34,46 @@ public class Solution {
 
 
     public String[] permutation(String s) {
-        List<String> result = new ArrayList<>();
-
+        Set<String> result = new HashSet<>();
+        char[] chars = s.toCharArray();
+        // 按照升序排序
+        Arrays.sort(chars);
+        boolean[] visit = new boolean[s.length()];
+        StringBuilder builder = new StringBuilder();
+        dfs(result, chars, visit, builder);
         String[] res = new String[result.size()];
-        for (int i = 0; i < result.size(); i++) {
-
+        int index = 0;
+        for (String element : result) {
+            res[index] = element;
+            index++;
         }
         return res;
+    }
+
+    /**
+     *
+     * @param result
+     * @param chars
+     * @param visit
+     */
+    private void dfs(Set<String> result, char[] chars, boolean[] visit, StringBuilder builder) {
+        if (builder.length() == visit.length) {
+            // 找到单词
+            result.add(builder.toString());
+        }
+        int n = visit.length;
+        // 遍历 visit 找到第一个没用的字符 使用
+        for (int i = 0; i < n; i++) {
+            if (visit[i]) {
+                continue;
+            }
+            // 使用这个字母 dfs
+            visit[i] = true;
+            builder.append(chars[i]);
+            dfs(result, chars, visit, builder);
+            builder.deleteCharAt(builder.length() - 1);
+            visit[i] = false;
+        }
     }
 
 }
