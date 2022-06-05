e7a256733c8240483d4ad03103ba2603501fc622
liuzhao11 <liuzhao11@sankuai.com>
2022-06-05 14:09:01 +0800

    2022.6.5 327. 区间和的个数 2024.11.16

--- src/main/java/com/potato/study/leetcodecn/p00327/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00327/t001/Solution.java
@@ -1,5 +1,6 @@
 package com.potato.study.leetcodecn.p00327.t001;
 
+
 import org.junit.Assert;
 
 /**
@@ -36,8 +37,130 @@ import org.junit.Assert;
 public class Solution {
 
 
-    // 327
+    private int lower;
+    private int upper;
+
+
+    /**
+     * https://leetcode.cn/problems/count-of-range-sum/solution/qu-jian-he-de-ge-shu-by-leetcode-solution/
+     * @param nums
+     * @param lower
+     * @param upper
+     * @return
+     */
     public int countRangeSum(int[] nums, int lower, int upper) {
-        return -1;
+        // 求数组的前缀和
+        int n = nums.length;
+        // 处理 0的情况 因为可以是整个数组
+        long[] prefixSum = new long[n+1];
+        for (int i = 1; i < prefixSum.length; i++) {
+            prefixSum[i] = prefixSum[i-1] + nums[i-1];
+        }
+        // 分治 归并排序
+        this.lower = lower;
+        this.upper = upper;
+        int left = 0;
+        int right = n;
+        // 每次归并合并时 进行计算 对于每一一个左边的位置 找到右边的开始和结束位置 减去左边位置的和 为  [lower, upper]  的 计算个数
+        return mergeAndSort(prefixSum, left, right);
+    }
+
+    /**
+     *
+     * @param prefixSum
+     * @param left
+     * @param right
+     */
+    private int mergeAndSort(long[] prefixSum, int left, int right) {
+        if (left == right) {
+            return 0;
+        }
+        // 使用mid 分割
+        int mid = (left + right) / 2;
+        int leftRes = mergeAndSort(prefixSum, left, mid);
+        int rightRes = mergeAndSort(prefixSum, mid + 1, right);
+
+        // 业务逻辑 这里 左边和右边有序 枚举左边每个位置作为开始位置
+        int total = leftRes + rightRes;
+        int start = mid + 1;
+        int end = mid + 1;
+        for (int i = left; i <= mid; i++) {
+            //  在右边找到窗口
+            while (start <= right && prefixSum[start] - prefixSum[i] < lower) {
+                start++;
+            }
+            while (end <= right && prefixSum[end] - prefixSum[i] <= upper) {
+                end++;
+            }
+
+            if (end >= start) {
+                total += (end - start);
+            }
+
+        }
+        // 归并逻辑
+        merge(prefixSum, left, right, mid);
+        return total;
+
+    }
+
+    /**
+     * 归并 【left，mid】， 【mid+1, right】
+     * 并对 left 到 right 排序
+     * @param prefixSum
+     * @param left
+     * @param right
+     * @param mid
+     */
+    private void merge(long[] prefixSum, int left, int right, int mid) {
+        // 先将 left 到 right copy出来
+        long[] temp = new long[right - left + 1];
+        for (int i = left; i <= right; i++) {
+            temp[i - left] = prefixSum[i];
+        }
+
+        int i = left;
+        int j = mid + 1;
+        // 枚举 left 和 right 每个位置
+        for (int k = left; k <= right; k++) {
+            if (i > mid) {
+                // i 消耗完了
+                prefixSum[k] = temp[j-left];
+                j++;
+            } else if (j > right) {
+                // j 消耗完了
+                prefixSum[k] = temp[i-left];
+                i++;
+            } else {
+                // ij 都没有消耗完
+                if (temp[i-left] <= temp[j-left]) {
+                    prefixSum[k] = temp[i-left];
+                    i++;
+                } else {
+                    prefixSum[k] = temp[j-left];
+                    j++;
+                }
+
+            }
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] nums = new int[]{-2,5,-1};
+        int lower = -2;
+        int upper = 2;
+        int i = solution.countRangeSum(nums, lower, upper);
+        System.out.println(i);
+        Assert.assertEquals(3, i);
+
+
+        nums = new int[]{0};
+        lower = 0;
+        upper = 0;
+        i = solution.countRangeSum(nums, lower, upper);
+        System.out.println(i);
+        Assert.assertEquals(1, i);
     }
 }
