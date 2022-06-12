95877c3a19196f923b429f98b4d439464d65e16f
liuzhao03 <liuzhao03@kuaishou.com>
2022-06-12 23:52:17 +0800

    2022.6.12 332. 重新安排行程 2024.12.7

--- src/main/java/com/potato/study/leetcodecn/p00332/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00332/t001/Solution.java
@@ -1,11 +1,17 @@
 package com.potato.study.leetcodecn.p00332.t001;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
 
 /**
  * 332. 重新安排行程
  *
- * 给定一个机票的字符串二维数组 [from, to]，子数组中的两个成员分别表示飞机出发和降落的机场地点，对该行程进行重新规划排序。所有这些机票都属于一个从 JFK（肯尼迪国际机场）出发的先生，所以该行程必须从 JFK 开始。
+ * 给定一个机票的字符串二维数组 [from, to]，子数组中的两个成员分别表示飞机出发和降落的机场地点，对该行程进行重新规划排序。
+ * 所有这些机票都属于一个从 JFK（肯尼迪国际机场）出发的先生，所以该行程必须从 JFK 开始。
  *
  *  
  *
@@ -44,7 +50,29 @@ public class Solution {
      * @return
      */
     public List<String> findItinerary(List<List<String>> tickets) {
+        // tickets 转化成 map key prio 结构
+        Map<String, PriorityQueue<String>> map = new HashMap<>();
+        for (List<String> ticket : tickets) {
+            String from = ticket.get(0);
+            String to = ticket.get(1);
+            if (!map.containsKey(from)) {
+                map.put(from, new PriorityQueue<>());
+            }
+            map.get(from).add(to);
+        }
+        // dfs 每次 删除节点 使用list存储
+        List<String> list = new ArrayList<>();
+        dfs(map, list, "JFK");
+        // 反转list
+        Collections.reverse(list);
+        return list;
+    }
 
-        return null;
+    private void dfs(Map<String, PriorityQueue<String>> map, List<String> list, String from) {
+        while (map.containsKey(from) && map.get(from).size() > 0) {
+            String to = map.get(from).poll();
+            dfs(map, list, to);
+        }
+        list.add(from);
     }
 }
