93aa9137438441110db6906496a020eca55fef98
liuzhao03 <liuzhao03@kuaishou.com>
2022-07-19 21:34:12 +0800

    2022.7.19 1676. 二叉树的最近公共祖先 IV 2025.4.21

---
+++ src/main/java/com/potato/study/leetcode/domain/val/left/right/parent/Node.java
@@ -0,0 +1,12 @@
+package com.potato.study.leetcode.domain.val.left.right.parent;
+
+/**
+ * @author liuzhao03 <liuzhao03@kuaishou.com>
+ * Created on 2022-07-19
+ */
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node parent;
+}
