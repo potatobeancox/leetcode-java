985b4a11acafde845ca9cc2ef6c2deae48c024ff
liuzhao03 <liuzhao03@kuaishou.com>
2022-07-20 17:33:04 +0800

    2022.7.19 1660. 纠正二叉树 2025.4.23

---
+++ src/main/java/com/potato/study/leetcodecn/p00253/t001/Solution.java
@@ -0,0 +1,45 @@
+package com.potato.study.leetcodecn.p00253.t001;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.potato.study.leetcode.domain.TreeNode;
+
+/**
+ * 253. 会议室 II
+ *
+ * 给你一个会议时间安排的数组 intervals ，每个会议时间都会包括开始和结束的时间 intervals[i] = [starti, endi] ，返回 所需会议室的最小数量 。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：intervals = [[0,30],[5,10],[15,20]]
+ * 输出：2
+ * 示例 2：
+ *
+ * 输入：intervals = [[7,10],[2,4]]
+ * 输出：1
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= intervals.length <= 104
+ * 0 <= starti < endi <= 106
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/meeting-rooms-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    public int minMeetingRooms(int[][] intervals) {
+        // 差分数组 10——00000
+        int[] count = new int[0];
+        return -1;
+    }
+
+
+}
