6024e70ae8362e1189c81372b69de532a34bf526
liuzhao03 <liuzhao03@kuaishou.com>
2022-07-27 20:56:23 +0800

    2022.7.27 280. 摆动排序 2025.5.33

--- src/main/java/com/potato/study/leetcodecn/p00280/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00280/t001/Solution.java
@@ -41,6 +41,16 @@ import java.util.List;
 public class Solution {
 
     public void wiggleSort(int[] nums) {
-
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (i % 2 == 0 && nums[i] > nums[i+1]) {
+                int tmp = nums[i];
+                nums[i] = nums[i+1];
+                nums[i+1] = tmp;
+            } else if (i % 2 == 1 && nums[i] < nums[i+1]) {
+                int tmp = nums[i];
+                nums[i] = nums[i+1];
+                nums[i+1] = tmp;
+            }
+        }
     }
 }
