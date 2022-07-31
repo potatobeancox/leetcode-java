038f1db89fcd3ee2e83e650df7a2aba2595af0f2
liuzhao11 <liuzhao11@sankuai.com>
2022-07-31 16:55:09 +0800

    2022.7.30 1983. 范围和相等的最宽索引对 2025.6.28

---
+++ src/main/java/com/potato/study/leetcodecn/p00369/t001/Solution.java
@@ -0,0 +1,46 @@
+package com.potato.study.leetcodecn.p00369.t001;
+
+import com.potato.study.leetcode.domain.ListNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * 369. 给单链表加一
+ *
+ * 给定一个用链表表示的非负整数， 然后将这个整数 再加上 1 。
+
+ 这些数字的存储是这样的：最高位有效的数字位于链表的首位 head 。
+
+  
+
+ 示例 1:
+
+ 输入: head = [1,2,3]
+ 输出: [1,2,4]
+ 示例 2:
+
+ 输入: head = [0]
+ 输出: [1]
+  
+
+ 提示：
+
+ 链表中的节点数在 [1, 100] 的范围内。
+ 0 <= Node.val <= 9
+ 由链表表示的数字不包含前导零，除了零本身。
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode.cn/problems/plus-one-linked-list
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+
+    public ListNode plusOne(ListNode head) {
+
+        return null;
+    }
+}
