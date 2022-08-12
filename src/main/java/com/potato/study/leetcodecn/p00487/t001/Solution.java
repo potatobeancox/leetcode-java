08edab5f2a08fd340d323194e948ec1c2c0da69e
liuzhao03 <liuzhao03@kuaishou.com>
2022-08-12 14:00:20 +0800

    2022.8.11 487. 最大连续1的个数 II 2025.8.6

--- src/main/java/com/potato/study/leetcodecn/p00487/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00487/t001/Solution.java
@@ -38,8 +38,37 @@ public class Solution {
 
 
     public int findMaxConsecutiveOnes(int[] nums) {
+        int left = 0;
+        int zeroCount = 0;
+        int maxOneCount = 0;
+        int oneCount = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) {
+                zeroCount++;
+            }
+            oneCount++;
+            // 超过了 1个0 将 left 往右滑动到 去掉0
+            while (zeroCount > 1) {
+                if (nums[left] == 0) {
+                    zeroCount--;
+                }
+                oneCount--;
+                left++;
+            }
+            // 窗口内部的max
+            maxOneCount = Math.max(maxOneCount, oneCount);
+        }
+        return maxOneCount;
+    }
 
-        return -1;
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[] arr = new int[] {
+                1,0,1,1,0
+        };
+        int maxConsecutiveOnes = solution.findMaxConsecutiveOnes(arr);
+        System.out.println(maxConsecutiveOnes);
+        Assert.assertEquals(4, maxConsecutiveOnes);
     }
 
 }
