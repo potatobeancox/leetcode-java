22fd6af55c702c7d4ed880bda4b2ef43656de5e3
liuzhao03 <liuzhao03@kuaishou.com>
2022-08-18 21:25:49 +0800

    2022.8.18 294. 翻转游戏 II 2025.8.24

---
+++ src/main/java/com/potato/study/leetcodecn/p00291/t001/Solution.java
@@ -0,0 +1,50 @@
+package com.potato.study.leetcodecn.p00291.t001;
+
+
+import org.junit.Assert;
+
+/**
+ * 291. 单词规律 II
+ *
+ * 给你一种规律 pattern 和一个字符串 s，请你判断 s 是否和 pattern 的规律相匹配。
+ *
+ * 如果存在单个字符到字符串的 双射映射 ，那么字符串 s 匹配 pattern ，即：如果pattern 中的每个字符都被它映射到的字符串替换，那么最终的字符串则为 s 。双射
+ * 意味着映射双方一一对应，不会存在两个字符映射到同一个字符串，也不会存在一个字符分别映射到两个不同的字符串。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：pattern = "abab", s = "redblueredblue"
+ * 输出：true
+ * 解释：一种可能的映射如下：
+ * 'a' -> "red"
+ * 'b' -> "blue"
+ * 示例 2：
+ *
+ * 输入：pattern = "aaaa", s = "asdasdasdasd"
+ * 输出：true
+ * 解释：一种可能的映射如下：
+ * 'a' -> "asd"
+ * 示例 3：
+ *
+ * 输入：pattern = "aabb", s = "xyzabcxzyabc"
+ * 输出：false
+ *  
+ *
+ * 提示：
+ *
+ * 1 <= pattern.length, s.length <= 20
+ * pattern 和 s 由小写英文字母组成
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/word-pattern-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+    public boolean wordPatternMatch(String pattern, String s) {
+
+        return false;
+    }
+}
