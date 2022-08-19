ace8bbd8c16cd90c7f8949b3ea9387738f1f5840
liuzhao11 <liuzhao11@sankuai.com>
2022-08-20 07:19:52 +0800

    2022.8.16 663. 均匀树划分 2025.8.17

--- src/main/java/com/potato/study/leetcodecn/p00291/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00291/t001/Solution.java
@@ -3,6 +3,9 @@ package com.potato.study.leetcodecn.p00291.t001;
 
 import org.junit.Assert;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 291. 单词规律 II
  *
@@ -43,8 +46,74 @@ import org.junit.Assert;
  *
  */
 public class Solution {
-    public boolean wordPatternMatch(String pattern, String s) {
 
+
+    /**
+     * https://leetcode.cn/problems/word-pattern-ii/solution/291-dan-ci-gui-lu-ii-by-klb/
+     * @param pattern
+     * @param s
+     * @return
+     */
+    public boolean wordPatternMatch(String pattern, String s) {
+        // 回溯 使用 map 记录 char word 关系 注意双射性质
+        Map<Character, String> mapping = new HashMap<>();
+        return backtrack(pattern, s, mapping);
+    }
+
+    private boolean backtrack(String pattern, String s, Map<Character, String> mapping) {
+        // 终止条件 pattern 找完了 看下 s 是不是也找完了
+        if (pattern == null || pattern.length() == 0) {
+            return s == null || s.length() == 0;
+        }
+        // 如果当前 s 剩余长度超过 patteren 那么直接赶回false 剪枝
+        if (pattern.length() > s.length()) {
+            return false;
+        }
+        // 没有终止 对于 pattern 0 依次找到 s 中可能的子串
+        char keyChar = pattern.charAt(0);
+        // i 控制子串长度 最长不能超过 s
+        for (int i = 1; i <= s.length(); i++) {
+            String subWord = s.substring(0, i);
+            // 去map 中看 有没有 没有的话 保证 value 也没有 ，有的话 看看 value 是不是一致
+            if (!mapping.containsKey(keyChar)) {
+                if (mapping.containsValue(subWord)) {
+                    continue;
+                }
+                // map 中没有 key 也没有 word
+                mapping.put(keyChar, subWord);
+                // 递归查找
+                boolean backtrack = backtrack(pattern.substring(1), s.substring(i), mapping);
+                if (backtrack) {
+                    return true;
+                }
+                // 重置状态
+                mapping.remove(keyChar);
+            } else {
+                // 已经包含了 看下是否可以匹配
+                if (subWord.equals(mapping.get(keyChar))) {
+                    // 子串能匹配上 递归找
+                    boolean backtrack = backtrack(pattern.substring(1), s.substring(i), mapping);
+                    if (backtrack) {
+                        return true;
+                    }
+                } else {
+                    // 子串匹配不上直接 continue
+                    continue;
+                }
+            }
+        }
         return false;
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+//        "abab"
+//        "redblueredblue"
+        String pattern = "abab";
+        String s = "redblueredblue";
+        boolean b = solution.wordPatternMatch(pattern, s);
+        System.out.println(b);
+        Assert.assertEquals(true, b);
     }
 }
