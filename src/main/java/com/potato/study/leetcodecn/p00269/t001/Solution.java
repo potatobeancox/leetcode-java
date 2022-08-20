6d01dbffd5d8edd035fd0fa84d52bc7b0f0906f3
liuzhao11 <liuzhao11@sankuai.com>
2022-08-20 23:52:30 +0800

    2022.8.20 267. 回文排列 II 2025.8.27

--- src/main/java/com/potato/study/leetcodecn/p00269/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00269/t001/Solution.java
@@ -1,5 +1,9 @@
 package com.potato.study.leetcodecn.p00269.t001;
 
+import org.junit.Assert;
+
+import java.util.*;
+
 /**
  * 269. 火星词典
  *
@@ -44,8 +48,146 @@ package com.potato.study.leetcodecn.p00269.t001;
 public class Solution {
 
     public String alienOrder(String[] words) {
+        // 遍历 word 每个位置 生成一个 parent list child 的 list list
+        List<List<Character>> graph = new ArrayList<>();
+        // 生成孩子
+        for (int i = 0; i < 26; i++) {
+            graph.add(new ArrayList<>());
+        }
+        // 用一个 map 记录 目前每个字母的入度
+        Map<Character, Integer> indegreeMap = new HashMap<>();
+        // 初始化
+        for (String word : words) {
+            char[] chars = word.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                indegreeMap.put(chars[i], 0);
+            }
+        }
 
-        return null;
+
+        for (int i = 0; i < words.length; i++) {
+            for (int j = i+1; j < words.length; j++) {
+                // 从第一个位置开始
+                int index = 0;
+                while (index < words[i].length() && index < words[j].length()
+                        && words[i].charAt(index) == words[j].charAt(index)) {
+                    index++;
+                }
+                // 达到了 某一个的终点
+                if (index >= words[i].length() || index >= words[j].length()) {
+                    // 出现了 长的在短的前面
+                    if (words[i].length() > words[j].length()) {
+                        return "";
+                    }
+                    break;
+                }
+                // 第一个不相同位置
+                char parent = words[i].charAt(index);
+                char child = words[j].charAt(index);
+
+                // 如果当前 child 出现过 前一个位置 相同那么就不
+                if (j > i+1 && words[j-1].length() > index
+                        && words[j-1].charAt(index) == child) {
+                    // 记录过关系
+                    continue;
+                }
+                // 计算对应关系
+                int parentIndex = parent - 'a';
+                graph.get(parentIndex).add(child);
+
+                // 计算入度
+                int indegree = indegreeMap.getOrDefault(child, 0);
+                indegree++;
+                indegreeMap.put(child, indegree);
+            }
+        }
+
+
+        // 将 map 入入度为 0的 插入 queue中
+        Queue<Character> queue = new LinkedList<>();
+        // 用 set 记录已经用过的字母 再次出现 直接返回false
+        Set<Character> usedSet = new HashSet<>();
+        for (Map.Entry<Character, Integer> entry : indegreeMap.entrySet()) {
+            int indegree = entry.getValue();
+            char ch = entry.getKey();
+            if (indegree == 0) {
+                queue.add(ch);
+                usedSet.add(ch);
+            }
+        }
+        // 每次从 queue 中 pop 出一个 字母 作为当前字母，修改 入度，如果修改之后 入度为 0 加入 queue
+        StringBuilder builder = new StringBuilder();
+        while (!queue.isEmpty()) {
+            char poll = queue.poll();
+            // 记录bulid
+            builder.append(poll);
+            // 修改 入度
+            int pollIndex = poll - 'a';
+            List<Character> childList = graph.get(pollIndex);
+            for (char child : childList) {
+                // 已经用过了的 child 说明 出现了环
+                if (usedSet.contains(child)) {
+                    return "";
+                }
+                // 修改入度
+                if (!indegreeMap.containsKey(child)
+                        || indegreeMap.get(child) <= 0) {
+                    return "";
+                }
+                indegreeMap.put(child, indegreeMap.get(child) - 1);
+                // 判断是否 进去queue 修改 set
+                if (indegreeMap.get(child) == 0) {
+                    queue.add(child);
+                    usedSet.add(child);
+                }
+            }
+        }
+        // 有些字符没法确定
+        if (builder.length() < indegreeMap.size()) {
+            return "";
+        }
+        return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String[] words = new String[] {
+                "wrt","wrf","er","ett","rftt"
+        };
+        String s = solution.alienOrder(words);
+        System.out.println(s);
+        Assert.assertEquals("wertf", s);
+
+
+        words = new String[] {
+                "z","z"
+        };
+        s = solution.alienOrder(words);
+        System.out.println(s);
+        Assert.assertEquals("z", s);
+
+        words = new String[] {
+                "ab","adc"
+        };
+        s = solution.alienOrder(words);
+        System.out.println(s);
+        Assert.assertEquals("abcd", s);
+
+
+        words = new String[] {
+                "abc","ab"
+        };
+        s = solution.alienOrder(words);
+        System.out.println(s);
+        Assert.assertEquals("", s);
+
+
+        words = new String[] {
+                "z","x","a","zb","zx"
+        };
+        s = solution.alienOrder(words);
+        System.out.println(s);
+        Assert.assertEquals("", s);
     }
 
 
