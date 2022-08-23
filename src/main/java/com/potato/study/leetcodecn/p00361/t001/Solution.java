b89669812a80cb7b65787a3aa654d8257045259f
liuzhao03 <liuzhao03@kuaishou.com>
2022-08-23 21:27:20 +0800

    2022.8.23 362. 敲击计数器 2025.9.19

---
+++ src/main/java/com/potato/study/leetcodecn/p00361/t001/Solution.java
@@ -0,0 +1,49 @@
+package com.potato.study.leetcodecn.p00361.t001;
+
+import java.util.Arrays;
+
+/**
+ * 361. 轰炸敌人
+ *
+ * 给你一个大小为 m x n 的矩阵 grid ，其中每个单元格都放置有一个字符：
+ *
+ * 'W' 表示一堵墙
+ * 'E' 表示一个敌人
+ * '0'（数字 0）表示一个空位
+ * 返回你使用 一颗炸弹 可以击杀的最大敌人数目。你只能把炸弹放在一个空位里。
+ *
+ * 由于炸弹的威力不足以穿透墙体，炸弹只能击杀同一行和同一列没被墙体挡住的敌人。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ *
+ * 输入：grid = [["0","E","0","0"],["E","0","W","E"],["0","E","0","0"]]
+ * 输出：3
+ * 示例 2：
+ *
+ *
+ * 输入：grid = [["W","W","W"],["0","0","0"],["E","E","E"]]
+ * 输出：1
+ *  
+ *
+ * 提示：
+ *
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 500
+ * grid[i][j] 可以是 'W'、'E' 或 '0'
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/bomb-enemy
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    public int maxKilledEnemies(char[][] grid) {
+        return -1;
+    }
+}
+
