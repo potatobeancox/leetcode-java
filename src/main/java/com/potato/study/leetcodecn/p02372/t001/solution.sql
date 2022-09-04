8f75db1a501320bb90be7442f3e34e15f9450da0
liuzhao11 <liuzhao11@meituan.com>
2022-09-04 11:59:15 +0800

    2022.9.4 2372. 计算每个销售人员的影响力 2025.10.17

---
+++ src/main/java/com/potato/study/leetcodecn/p02372/t001/solution.sql
@@ -0,0 +1,107 @@
+-- 2372. 计算每个销售人员的影响力
+-- SQL架构
+-- 表： Salesperson
+--
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | salesperson_id | int     |
+-- | name           | varchar |
+-- +----------------+---------+
+-- sales_person_id 是这个表的主键。
+-- 这个表中的每一行都显示一个销售人员的 ID。
+--
+--
+-- 表：Customer
+--
+-- +----------------+------+
+-- | Column Name    | Type |
+-- +----------------+------+
+-- | customer_id    | int  |
+-- | salesperson_id | int  |
+-- +----------------+------+
+-- customer_id 是这个表的主键。
+-- salesperson_id 是一个来自于 Salesperson 表的外键
+-- Customer 表中的每一行都显示了一个客户的 ID 和销售人员的 ID。
+--
+--
+-- 表：Sales
+--
+-- +-------------+------+
+-- | Column Name | Type |
+-- +-------------+------+
+-- | sale_id     | int  |
+-- | customer_id | int  |
+-- | price       | int  |
+-- +-------------+------+
+-- sale_id 是这个表的主键。
+-- customer_id 是一个来自于 Customer 表的外键。
+-- Sales 表中的每一行都显示了一个客户的 ID 以及他们在 sale_id 指代的交易中所支付的金额。
+--
+--
+-- 编写一个 SQL 查询用来报告每个销售人员的客户所支付的价格总和。如果销售人员没有任何客户，则总值应该为 0 。
+-- 以 任意顺序 返回结果表。
+-- 查询结果格式如下所示。
+--
+--
+--
+-- 示例 1:
+--
+-- 输入:
+-- Salesperson 表:
+-- +----------------+-------+
+-- | salesperson_id | name  |
+-- +----------------+-------+
+-- | 1              | Alice |
+-- | 2              | Bob   |
+-- | 3              | Jerry |
+-- +----------------+-------+
+-- Customer 表:
+-- +-------------+----------------+
+-- | customer_id | salesperson_id |
+-- +-------------+----------------+
+-- | 1           | 1              |
+-- | 2           | 1              |
+-- | 3           | 2              |
+-- +-------------+----------------+
+-- Sales 表:
+-- +---------+-------------+-------+
+-- | sale_id | customer_id | price |
+-- +---------+-------------+-------+
+-- | 1       | 2           | 892   |
+-- | 2       | 1           | 354   |
+-- | 3       | 3           | 988   |
+-- | 4       | 3           | 856   |
+-- +---------+-------------+-------+
+-- 输出:
+-- +----------------+-------+-------+
+-- | salesperson_id | name  | total |
+-- +----------------+-------+-------+
+-- | 1              | Alice | 1246  |
+-- | 2              | Bob   | 1844  |
+-- | 3              | Jerry | 0     |
+-- +----------------+-------+-------+
+-- 解释:
+-- Alice 是客户 1 和客户 2 的销售人员。
+--   - 客户 1 一次购买花费了 354。
+--   - 客户 2 一次购买花费了 892。
+-- Alice 的总数是 354 + 892 = 1246。
+--
+-- Bob 是客户 3 的销售人员。
+--   - 客户 3 一次购买花费了 988，另一次购买花费了 856。
+-- Bob 的总数是 988 + 856 = 1844。
+--
+-- Jerry 没有客户。
+-- Jerry 的总数是 0。
+
+
+-- https://leetcode.cn/problems/calculate-the-influence-of-each-salesperson/solution/by-btnthzg5ht-6djh/
+-- 两次链接
+select
+Salesperson.salesperson_id as salesperson_id,
+Salesperson.name as name,
+ifnull(sum(Sales.price), 0) as total
+from Salesperson
+left join Customer using(salesperson_id)
+left join Sales using(customer_id)
+group by Salesperson.salesperson_id
