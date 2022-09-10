4f0a36bb7d793e4e1c58d1f17952fa84f50c6322
liuzhao11 <liuzhao11@meituan.com>
2022-09-10 20:57:12 +0800

    2022.9.8 761. 特殊的二进制序列 2025.10.28

--- src/main/java/com/potato/study/leetcodecn/p00761/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00761/t001/Solution.java
@@ -1,9 +1,8 @@
 package com.potato.study.leetcodecn.p00761.t001;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import org.junit.Assert;
+
+import java.util.*;
 
 /**
  * 761. 特殊的二进制序列
@@ -35,8 +34,53 @@ import java.util.Map;
  */
 public class Solution {
 
+    /**
+     * https://leetcode.cn/problems/special-binary-string/solution/by-ac_oier-cz6h/
+     * @param s
+     * @return
+     */
     public String makeLargestSpecial(String s) {
+        // 使用 一个 k 记录 当前子串的得分，
+        int status = 0;
+        // 遍历 s 记录 当前没有改动的开始位置 和结束位置， 对于之间的位置 地柜进行比较
+        int start = 0;
+        char[] chars = s.toCharArray();
+        // 用list 生成一个 每次 k等于 0生成的子串
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < chars.length; i++) {
+            if (chars[i] == '0') {
+                status--;
+            } else {
+                // 1
+                status++;
+            }
+            // 当前是否满足等于 0
+            if (status == 0) {
+                String subString = s.substring(start + 1, i+1);
+                String temp = "1" + makeLargestSpecial(subString) + "0";
+                list.add(temp);
+                // 重新计算 开始位置
+                start = i + 1;
+            }
+        }
+        // 对于 list 进行排序 字典序升序
+        Collections.sort(list, (o1, o2) -> {
+            String tmp = o1 + o2;
+            return tmp.compareTo(o2 + o1);
+        });
+        // 构造结果
+        StringBuilder builder = new StringBuilder();
+        for (int i = list.size() - 1; i >= 0; i--) {
+            builder.append(list.get(i));
+        }
+        return builder.toString();
+    }
 
-        return null;
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "11011000";
+        String str = solution.makeLargestSpecial(s);
+        System.out.println(str);
+        Assert.assertEquals("11100100", str);
     }
 }
