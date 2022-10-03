73d171aba985d87c6ba6a49dd65dbcca8c26f3bb
liuzhao11 <liuzhao11@meituan.com>
2022-10-03 14:35:49 +0800

    2022.10.1 1087. 花括号展开 2025.12.31

--- src/main/java/com/potato/study/leetcodecn/p01087/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p01087/t001/Solution.java
@@ -1,8 +1,6 @@
 package com.potato.study.leetcodecn.p01087.t001;
 
-import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.TreeMap;
+import java.util.*;
 
 /**
  * 1087. 花括号展开
@@ -43,7 +41,86 @@ import java.util.TreeMap;
 public class Solution {
 
     public String[] expand(String s) {
+        // 回溯生成
+        List<String> resultList = new ArrayList<>();
+        List<String> tempList = new ArrayList<>();
+        backtrack(s, 0, resultList, tempList);
+        // list -> array
+        String[] resultArray = new String[resultList.size()];
+        for (int i = 0; i < resultList.size(); i++) {
+            resultArray[i] = resultList.get(i);
+        }
+        Arrays.sort(resultArray);
+        return resultArray;
+    }
 
-        return null;
+    /**
+     *
+     * @param s
+     * @param index     本次开始遍历的index
+     * @param resultList 结果集
+     * @param tempList 当前生成的字符串
+     */
+    private void backtrack(String s, int index, List<String> resultList, List<String> tempList) {
+        // 终止条件 当前index 已经到了最后
+        if (index >= s.length()) {
+            resultList.addAll(tempList);
+            return;
+        }
+        // 如果当前字幕 是，  {a,b,c}  往下一个字幕 如果是 字母 往结果集合中添加一下 否则 是 { 找到下一个 }
+        char c = s.charAt(index);
+        if (c == ',') {
+            index++;
+            c = s.charAt(index);
+        }
+        if (Character.isAlphabetic(c)) {
+            // 修改 temp
+            List<String> nextList = new ArrayList<>();
+            if (tempList.size() == 0) {
+                nextList.add(String.valueOf(c));
+            } else {
+                for (String temp : tempList) {
+                    nextList.add(temp + c);
+                }
+            }
+            backtrack(s, index + 1, resultList, nextList);
+        } else {
+            // { find }
+            int leftIndex = index;
+            //  对于 「」 之前字母 按照 ， split 分别组装的 currentList 上面
+            int p = index;
+            while (p < s.length() && s.charAt(p) != '}') {
+                p++;
+            }
+            // 边界
+            if (p >= s.length() || s.charAt(p) != '}') {
+                throw new RuntimeException("{}字符串非法");
+            }
+            int rightIndex = p;
+            String charListStr = s.substring(leftIndex + 1, rightIndex);
+            String[] split = charListStr.split(",");
+            // 每个元素是一个 字母
+            List<String> nextList = new ArrayList<>();
+            for (String charString : split) {
+                // 如果是 空的话 直接添加
+                if (tempList.size() == 0) {
+                    nextList.add(charString);
+                    continue;
+                }
+                // 遍历每个结果
+                for (String temp : tempList) {
+                    nextList.add(temp + charString);
+                }
+            }
+            // 下一个位置的结果
+            backtrack(s, rightIndex + 1, resultList, nextList);
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String input = "{a,b}c{d,e}f";
+        String[] expand = solution.expand(input);
+        System.out.println(Arrays.toString(expand));
     }
 }
