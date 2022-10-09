16645e9869115c990551b6bf3abebbe52bd77a42
liuzhao11 <liuzhao11@sankuai.com>
2022-10-09 12:05:41 +0800

    2022.10.9 2348. 全 0 子数组的数目 2026.2.15

--- src/main/java/com/potato/study/leetcodecn/p02348/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p02348/t001/Solution.java
@@ -1,62 +1,69 @@
 package com.potato.study.leetcodecn.p02348.t001;
 
 /**
- * 2378. Choose Edges to Maximize Score in a Tree
+ * 2348. 全 0 子数组的数目
  *
- * You are given a weighted tree consisting of n nodes numbered from 0 to n - 1.
+ * 给你一个整数数组 nums ，返回全部为 0 的 子数组 数目。
 
- The tree is rooted at node 0 and represented with a 2D array edges of size n where edges[i] = [pari, weighti] indicates that node pari is the parent of node i, and the edge between them has a weight equal to weighti. Since the root does not have a parent, you have edges[0] = [-1, -1].
+ 子数组 是一个数组中一段连续非空元素组成的序列。
 
- Choose some edges from the tree such that no two chosen edges are adjacent and the sum of the weights of the chosen edges is maximized.
-
- Return the maximum sum of the chosen edges.
-
- Note:
-
- You are allowed to not choose any edges in the tree, the sum of weights in this case will be 0.
- Two edges Edge1 and Edge2 in the tree are adjacent if they have a common node.
- In other words, they are adjacent if Edge1 connects nodes a and b and Edge2 connects nodes b and c.
   
 
- Example 1:
+ 示例 1：
 
+ 输入：nums = [1,3,0,0,2,0,0,4]
+ 输出：6
+ 解释：
+ 子数组 [0] 出现了 4 次。
+ 子数组 [0,0] 出现了 2 次。
+ 不存在长度大于 2 的全 0 子数组，所以我们返回 6 。
+ 示例 2：
 
- Input: edges = [[-1,-1],[0,5],[0,10],[2,6],[2,4]]
- Output: 11
- Explanation: The above diagram shows the edges that we have to choose colored in red.
- The total score is 5 + 6 = 11.
- It can be shown that no better score can be obtained.
- Example 2:
+ 输入：nums = [0,0,0,2,0,0]
+ 输出：9
+ 解释：
+ 子数组 [0] 出现了 5 次。
+ 子数组 [0,0] 出现了 3 次。
+ 子数组 [0,0,0] 出现了 1 次。
+ 不存在长度大于 3 的全 0 子数组，所以我们返回 9 。
+ 示例 3：
 
-
- Input: edges = [[-1,-1],[0,5],[0,-6],[0,7]]
- Output: 7
- Explanation: We choose the edge with weight 7.
- Note that we cannot choose more than one edge because all edges are adjacent to each other.
+ 输入：nums = [2,10,2019]
+ 输出：0
+ 解释：没有全 0 子数组，所以我们返回 0 。
   
 
- Constraints:
+ 提示：
 
- n == edges.length
- 1 <= n <= 105
- edges[i].length == 2
- par0 == weight0 == -1
- 0 <= pari <= n - 1 for all i >= 1.
- pari != i
- -106 <= weighti <= 106 for all i >= 1.
- edges represents a valid tree.
+ 1 <= nums.length <= 105
+ -109 <= nums[i] <= 109
 
  来源：力扣（LeetCode）
- 链接：https://leetcode.cn/problems/choose-edges-to-maximize-score-in-a-tree
+ 链接：https://leetcode.cn/problems/number-of-zero-filled-subarrays
  著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
  */
 public class Solution {
 
 
-    public long maxScore(int[][] edges) {
+    public long zeroFilledSubarray(int[] nums) {
+        // 遍历 统计当前 全0 连续了多少
+        long count = 0;
+        long totalCount = 0;
+        for (int num : nums) {
+            if (num == 0) {
+                count++;
+            } else {
+                count = 0;
+            }
+            // 结算 以当前 0结尾的子串的个数
+            totalCount += count;
+        }
+        return totalCount;
+    }
 
-        return -1;
+    public static void main(String[] args) {
+
     }
 
 }
