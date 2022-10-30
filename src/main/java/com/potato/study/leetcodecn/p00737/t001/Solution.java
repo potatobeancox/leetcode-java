d78b34100747298b788956379882eb73c568fcbd
liuzhao11 <liuzhao11@sankuai.com>
2022-10-30 22:22:28 +0800

    2022.10.30 737. 句子相似性 II 2026.4.29

--- src/main/java/com/potato/study/leetcodecn/p00737/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p00737/t001/Solution.java
@@ -1,6 +1,11 @@
 package com.potato.study.leetcodecn.p00737.t001;
 
+import com.potato.study.leetcode.util.LeetcodeInputUtils;
+import org.junit.Assert;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * 737. 句子相似性 II
@@ -56,7 +61,102 @@ public class Solution {
 
     public boolean areSentencesSimilarTwo(String[] sentence1, String[] sentence2,
                                           List<List<String>> similarPairs) {
+        // 将 similarPairs 单词转换成数字
+        Map<String, Integer> wordIndexMap = new HashMap<>();
+        UnionFind unionFind = new UnionFind(similarPairs.size() * 2);
+        for (List<String> similarPair : similarPairs) {
+            String word1 = similarPair.get(0);
+            String word2 = similarPair.get(1);
 
-        return false;
+            // 两个字符串相似
+            int wordIndex1;
+            if (!wordIndexMap.containsKey(word1)) {
+                wordIndex1 = wordIndexMap.size();
+                wordIndexMap.put(word1, wordIndexMap.size());
+            } else {
+                wordIndex1 = wordIndexMap.get(word1);
+            }
+
+            int wordIndex2;
+            if (!wordIndexMap.containsKey(word2)) {
+                wordIndex2 = wordIndexMap.size();
+                wordIndexMap.put(word2, wordIndexMap.size());
+            } else {
+                wordIndex2 = wordIndexMap.get(word2);
+            }
+
+            unionFind.union(wordIndex1, wordIndex2);
+        }
+        // 分别对 sentence1 和 sentence2 每个单词进行find 找到对应结果
+        if (sentence1.length != sentence2.length) {
+            return false;
+        }
+        int n = sentence1.length;
+        for (int i = 0; i < n; i++) {
+            String word1 = sentence1[i];
+            String word2 = sentence2[i];
+
+
+            if (word1.equals(word2)) {
+                continue;
+            }
+
+            // 如果 map 中不包含某一个那就是不相似
+            if (!wordIndexMap.containsKey(word1)
+                    || !wordIndexMap.containsKey(word2)) {
+                return false;
+            }
+
+            int p1 = unionFind.find(wordIndexMap.get(word1));
+            int p2 = unionFind.find(wordIndexMap.get(word2));
+            // 没有相同父亲就是不相似
+            if (p1 != p2) {
+                return false;
+            }
+
+        }
+        return true;
+    }
+
+    class UnionFind {
+        private int[] parent;
+
+        public UnionFind(int n) {
+            this.parent = new int[n];
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        public void union(int target1, int target2) {
+            int p1 = find(target1);
+            int p2 = find(target2);
+
+            if (p1 == p2) {
+                return;
+            }
+            parent[p2] = p1;
+        }
+
+        public int find(int target) {
+            while (target != parent[target]) {
+                target = parent[target];
+            }
+            return target;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        String[] sentence1 = LeetcodeInputUtils.inputString2StringArray("[\"I\",\"love\",\"leetcode\"]");
+        String[] sentence2 = LeetcodeInputUtils.inputString2StringArray("[\"I\",\"love\",\"onepiece\"]");
+        String input = "[[\"manga\",\"onepiece\"],[\"platform\",\"anime\"],[\"leetcode\",\"platform\"],[\"anime\",\"manga\"]]";
+        List<List<String>> similarPairs = LeetcodeInputUtils.inputString2StringListList(input);
+
+        boolean b = solution.areSentencesSimilarTwo(sentence1, sentence2, similarPairs);
+        System.out.println(b);
+        Assert.assertEquals(true, b);
     }
 }
