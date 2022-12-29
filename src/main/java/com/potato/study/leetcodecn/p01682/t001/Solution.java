8a4f7e698caf6fa4767eb3781da1713d4cfc72cb
liuzhao11 <liuzhao11@sankuai.com>
2022-12-29 23:45:34 +0800

    2022.12.29 面试题 08.11. 硬币 2026.6.19

---
+++ src/main/java/com/potato/study/leetcodecn/p01682/t001/Solution.java
@@ -0,0 +1,48 @@
+package com.potato.study.leetcodecn.p01682.t001;
+
+/**
+ * 1682. 最长回文子序列 II
+ *
+ * 字符串 s 的某个子序列符合下列条件时，称为“好的回文子序列”：
+
+ 它是 s 的子序列。
+ 它是回文序列（反转后与原序列相等）。
+ 长度为偶数。
+ 除中间的两个字符外，其余任意两个连续字符不相等。
+ 例如，若 s = "abcabcabb"，则 "abba" 可称为“好的回文子序列”，而 "bcb" （长度不是偶数）和 "bbbb" （含有相等的连续字符）不能称为“好的回文子序列”。
+
+ 给定一个字符串 s， 返回 s 的最长“好的回文子序列”的长度。
+
+  
+
+ 示例 1:
+
+ 输入: s = "bbabab"
+ 输出: 4
+ 解释: s 的最长“好的回文子序列”是 "baab"。
+ 示例 2:
+
+ 输入: s = "dcbccacdb"
+ 输出: 4
+ 解释: s 的最长“好的回文子序列”是 "dccd"。
+  
+
+ 提示:
+
+ 1 <= s.length <= 250
+ s 包含小写英文字母。
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode.cn/problems/longest-palindromic-subsequence-ii
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    public int longestPalindromeSubseq(String s) {
+
+        return -1;
+    }
+
+
+}
