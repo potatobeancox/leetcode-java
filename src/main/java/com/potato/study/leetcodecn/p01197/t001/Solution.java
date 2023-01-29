ab207f2c2ce0da20905e33705c3dfff0dc6a02ec
liuzhao11 <liuzhao11@sankuai.com>
2023-01-29 16:46:19 +0800

    2022.12.30 1197. 进击的骑士 2026.8.7

--- src/main/java/com/potato/study/leetcodecn/p01197/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/p01197/t001/Solution.java
@@ -1,7 +1,9 @@
 package com.potato.study.leetcodecn.p01197.t001;
 
 
-import java.util.Arrays;
+import org.junit.Assert;
+
+import java.util.*;
 
 /**
  * 1197. 进击的骑士
@@ -43,9 +45,75 @@ import java.util.Arrays;
 public class Solution {
 
     public int minKnightMoves(int x, int y) {
+        if (x == 0 && y == 0) {
+            return 0;
+        }
+        // 将 xy 转化到 第一象限
+        x = Math.abs(x);
+        y = Math.abs(y);
+        // bfs 开始将 原点移动 50 50
+        x += 50;
+        y += 50;
+        // 从 00 开始 bfs 相当于 从50，50开始 bfs
+        Queue<int[]> queue = new LinkedList<>();
+        // arr[2] 记录当前位置用了多少步骤
+        queue.add(new int[] {50, 50, 0});
+        // 各个方向
+        int[][] direction = new int[][] {
+                {1, 2},
+                {-1, 2},
+                {-1, -2},
+                {1, -2},
+                {2, 1},
+                {-2, 1},
+                {-2, -1},
+                {2, -1}
+        };
+        Set<String> visited = new HashSet<>();
+        visited.add(50 + "_" + 50);
+        int step = -1;
+        while (!queue.isEmpty()) {
+            int[] poll = queue.poll();
+            int dx = poll[0];
+            int dy = poll[1];
+            int cost = poll[2];
+            // 往8个方向找
+            for (int i = 0; i < 8; i++) {
+                int nextX = dx + direction[i][0];
+                int nextY = dy + direction[i][1];
+                String key = nextX + "_" + nextY;
+                if (visited.contains(key)) {
+                    continue;
+                }
+                if (nextX < 0 || nextY < 0) {
+                    continue;
+                }
+                int[] nextNode = new int[] {nextX, nextY, cost + 1};
+                if (nextX == x && nextY == y) {
+                    return nextNode[2];
+                }
+                queue.add(nextNode);
+                visited.add(key);
+            }
+
+        }
+        return step;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int i = solution.minKnightMoves(2, 1);
+        System.out.println(i);
+        Assert.assertEquals(1, i);
 
 
+        i = solution.minKnightMoves(5, 5);
+        System.out.println(i);
+        Assert.assertEquals(4, i);
 
-        return -1;
+
+        i = solution.minKnightMoves(0, 0);
+        System.out.println(i);
+        Assert.assertEquals(0, i);
     }
 }
