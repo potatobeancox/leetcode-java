8fd689c8bda2f3206afa53242fe0fe246ccb5364
liuzhao11 <liuzhao11@sankuai.com>
2023-03-26 00:28:03 +0800

    2023.3.20 2594. 修车的最少时间 2026.11.13

---
+++ src/main/java/com/potato/study/leetcodecn/p02591/t001/Solution.java
@@ -0,0 +1,94 @@
+package com.potato.study.leetcodecn.p02591.t001;
+
+import org.junit.Assert;
+
+/**
+ * 2591. 将钱分给最多的儿童
+
+ 给你一个整数 money ，表示你总共有的钱数（单位为美元）和另一个整数 children ，表示你要将钱分配给多少个儿童。
+
+ 你需要按照如下规则分配：
+
+ 所有的钱都必须被分配。
+ 每个儿童至少获得 1 美元。
+ 没有人获得 4 美元。
+ 请你按照上述规则分配金钱，并返回 最多 有多少个儿童获得 恰好 8 美元。如果没有任何分配方案，返回 -1 。
+
+  
+
+ 示例 1：
+
+ 输入：money = 20, children = 3
+ 输出：1
+ 解释：
+ 最多获得 8 美元的儿童数为 1 。一种分配方案为：
+ - 给第一个儿童分配 8 美元。
+ - 给第二个儿童分配 9 美元。
+ - 给第三个儿童分配 3 美元。
+ 没有分配方案能让获得 8 美元的儿童数超过 1 。
+ 示例 2：
+
+ 输入：money = 16, children = 2
+ 输出：2
+ 解释：每个儿童都可以获得 8 美元。
+  
+
+ 提示：
+
+ 1 <= money <= 200
+ 2 <= children <= 30
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode.cn/problems/distribute-money-to-maximum-children
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+
+    // 2591
+    public int distMoney(int money, int children) {
+        // 所有的钱都必须被分配。
+        //// 每个儿童至少获得 1 美元。
+        // 如果当前 money 小于 children数量 返回 -1
+        if (money < children) {
+            return -1;
+        }
+        int remind = money - children;
+        if (remind == 0) {
+            return 0;
+        }
+        // 否则 先按照每个人1块钱 分 剩余 money 计算一下
+        int maxEightCount = remind / 7;
+        if (maxEightCount > children) {
+            return children - 1;
+        }
+        int notUseMoney = remind % 7;
+        if (maxEightCount == children) {
+            if (notUseMoney == 0) {
+                return children;
+            } else {
+                return children - 1;
+            }
+        }
+        if (notUseMoney == 0) {
+            return maxEightCount;
+        }
+        // 还有人 不是 8
+        if (notUseMoney == 3) {
+            return maxEightCount - 1;
+        }
+        return maxEightCount;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int money = 5;
+        int children = 2;
+        int i = solution.distMoney(money, children);
+        System.out.println(i);
+        Assert.assertEquals(0, i);
+    }
+
+
+}
---
+++ src/main/java/com/potato/study/leetcodecn/p02593/t001/Solution.java
@@ -0,0 +1,80 @@
+package com.potato.study.leetcodecn.p02593.t001;
+
+import java.util.PriorityQueue;
+
+/**
+ * 2593. 标记所有元素后数组的分数
+
+ 给你一个数组 nums ，它包含若干正整数。
+
+ 一开始分数 score = 0 ，请你按照下面算法求出最后分数：
+
+ 从数组中选择最小且没有被标记的整数。如果有相等元素，选择下标最小的一个。
+ 将选中的整数加到 score 中。
+ 标记 被选中元素，如果有相邻元素，则同时标记 与它相邻的两个元素 。
+ 重复此过程直到数组中所有元素都被标记。
+ 请你返回执行上述算法后最后的分数。
+
+  
+
+ 示例 1：
+
+ 输入：nums = [2,1,3,4,5,2]
+ 输出：7
+ 解释：我们按照如下步骤标记元素：
+ - 1 是最小未标记元素，所以标记它和相邻两个元素：[2,1,3,4,5,2] 。
+ - 2 是最小未标记元素，所以标记它和左边相邻元素：[2,1,3,4,5,2] 。
+ - 4 是仅剩唯一未标记的元素，所以我们标记它：[2,1,3,4,5,2] 。
+ 总得分为 1 + 2 + 4 = 7 。
+ 示例 2：
+
+ 输入：nums = [2,3,5,1,3,2]
+ 输出：5
+ 解释：我们按照如下步骤标记元素：
+ - 1 是最小未标记元素，所以标记它和相邻两个元素：[2,3,5,1,3,2] 。
+ - 2 是最小未标记元素，由于有两个 2 ，我们选择最左边的一个 2 ，也就是下标为 0 处的 2 ，以及它右边相邻的元素：[2,3,5,1,3,2] 。
+ - 2 是仅剩唯一未标记的元素，所以我们标记它：[2,3,5,1,3,2] 。
+ 总得分为 1 + 2 + 2 = 5 。
+  
+
+ 提示：
+
+ 1 <= nums.length <= 105
+ 1 <= nums[i] <= 106
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode.cn/problems/find-score-of-an-array-after-marking-all-elements
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+
+    // 2593
+    public long findScore(int[] nums) {
+        boolean[] used = new boolean[nums.length];
+        // 0：值， 1：index
+        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>((o1, o2) -> Integer.compare(o1[0], o2[0]));
+        for (int i = 0; i < nums.length; i++) {
+            priorityQueue.add(new int[] {nums[i], i});
+        }
+        long score = 0L;
+        while (!priorityQueue.isEmpty()) {
+            int[] poll = priorityQueue.poll();
+            int index = poll[1];
+            if (used[index]) {
+                continue;
+            }
+            used[index] = true;
+            score += poll[0];
+            if (index + 1 < nums.length) {
+                used[index+1] = true;
+            }
+            if (index - 1 >= 0) {
+                used[index-1] = true;
+            }
+        }
+        return score;
+    }
+
+}
