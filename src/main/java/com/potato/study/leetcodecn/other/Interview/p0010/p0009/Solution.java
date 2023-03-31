e4d84a3dd4f72196dc65e4b839ffbd15c6070187
liuzhao11 <liuzhao11@meituan.com>
2023-04-01 00:12:51 +0800

    2022.3.31 面试题 10.09. 排序矩阵查找 2026.11.26

---
+++ src/main/java/com/potato/study/leetcodecn/other/Interview/p0010/p0009/Solution.java
@@ -0,0 +1,50 @@
+package com.potato.study.leetcodecn.other.Interview.p0010.p0009;
+
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 面试题 10.09. 排序矩阵查找
+ *
+ * 给定M×N矩阵，每一行、每一列都按升序排列，请编写代码找出某元素。
+ *
+ * 示例:
+ *
+ * 现有矩阵 matrix 如下：
+ *
+ * [
+ *   [1,   4,  7, 11, 15],
+ *   [2,   5,  8, 12, 19],
+ *   [3,   6,  9, 16, 22],
+ *   [10, 13, 14, 17, 24],
+ *   [18, 21, 23, 26, 30]
+ * ]
+ * 给定 target = 5，返回 true。
+ *
+ * 给定 target = 20，返回 false。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/sorted-matrix-search-lcci
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+
+    // 10.09 定位左下角
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = matrix.length - 1;
+        int j = 0;
+        while (i >= 0 && j < matrix[0].length) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] > target) {
+                i--;
+            } else {
+                j++;
+            }
+        }
+        return false;
+    }
+}
