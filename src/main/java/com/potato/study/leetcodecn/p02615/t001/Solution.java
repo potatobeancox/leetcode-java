65fba74dec316116c59f07ce1bd3bc1e29444bcf
liuzhao11 <liuzhao11@sankuai.com>
2023-04-10 23:20:07 +0800

    2023.4.9 剑指 Offer II 057. 值和下标之差都在给定的范围内 2027.1.7

---
+++ src/main/java/com/potato/study/leetcodecn/p02615/t001/Solution.java
@@ -0,0 +1,61 @@
+package com.potato.study.leetcodecn.p02615.t001;
+
+import java.util.*;
+
+/**
+ *
+ * 2615. 等值距离和
+ *
+ * 给你一个下标从 0 开始的整数数组 nums 。现有一个长度等于 nums.length 的数组 arr 。对于满足 nums[j] == nums[i] 且 j != i 的所有 j ，arr[i] 等于所有 |i - j| 之和。如果不存在这样的 j ，则令 arr[i] 等于 0 。
+
+ 返回数组 arr 。
+
+  
+
+ 示例 1：
+
+ 输入：nums = [1,3,1,1,2]
+ 输出：[5,0,3,4,0]
+ 解释：
+ i = 0 ，nums[0] == nums[2] 且 nums[0] == nums[3] 。因此，arr[0] = |0 - 2| + |0 - 3| = 5 。
+ i = 1 ，arr[1] = 0 因为不存在值等于 3 的其他下标。
+ i = 2 ，nums[2] == nums[0] 且 nums[2] == nums[3] 。因此，arr[2] = |2 - 0| + |2 - 3| = 3 。
+ i = 3 ，nums[3] == nums[0] 且 nums[3] == nums[2] 。因此，arr[3] = |3 - 0| + |3 - 2| = 4 。
+ i = 4 ，arr[4] = 0 因为不存在值等于 2 的其他下标。
+ 示例 2：
+
+ 输入：nums = [0,5,3]
+ 输出：[0,0,0]
+ 解释：因为 nums 中的元素互不相同，对于所有 i ，都有 arr[i] = 0 。
+  
+
+ 提示：
+
+ 1 <= nums.length <= 105
+ 0 <= nums[i] <= 109
+
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode.cn/problems/sum-of-distances
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+
+    // 2615
+    public long[] distance(int[] nums) {
+        Map<Integer, List<Integer>> appearMap = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            List<Integer> indexList = appearMap.getOrDefault(nums[i], new ArrayList<>());
+            indexList.add(i);
+
+            appearMap.put(nums[i], indexList);
+        }
+        return null;
+    }
+
+
+
+
+}
---
+++ src/main/java/com/potato/study/leetcodecn/p02616/t001/Solution.java
@@ -0,0 +1,91 @@
+package com.potato.study.leetcodecn.p02616.t001;
+
+import java.util.Arrays;
+import java.util.PriorityQueue;
+
+/**
+ *
+ * 2616. 最小化数对的最大差值
+ *
+ * 给你一个下标从 0 开始的整数数组 nums 和一个整数 p 。请你从 nums 中找到 p 个下标对，每个下标对对应数值取差值，你需要使得这 p 个差值的 最大值 最小。同时，你需要确保每个下标在这 p 个下标对中最多出现一次。
+
+ 对于一个下标对 i 和 j ，这一对的差值为 |nums[i] - nums[j]| ，其中 |x| 表示 x 的 绝对值 。
+
+ 请你返回 p 个下标对对应数值 最大差值 的 最小值 。
+
+  
+
+ 示例 1：
+
+ 输入：nums = [10,1,2,7,1,3], p = 2
+ 输出：1
+ 解释：第一个下标对选择 1 和 4 ，第二个下标对选择 2 和 5 。
+ 最大差值为 max(|nums[1] - nums[4]|, |nums[2] - nums[5]|) = max(0, 1) = 1 。所以我们返回 1 。
+ 示例 2：
+
+ 输入：nums = [4,2,1,2], p = 1
+ 输出：0
+ 解释：选择下标 1 和 3 构成下标对。差值为 |2 - 2| = 0 ，这是最大差值的最小值。
+  
+
+ 提示：
+
+ 1 <= nums.length <= 105
+ 0 <= nums[i] <= 109
+ 0 <= p <= (nums.length)/2
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode.cn/problems/minimize-the-maximum-difference-of-pairs
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+
+    public int minimizeMax(int[] nums, int p) {
+        // 二分法 一次枚举 差 看是不是超过了 p个
+        Arrays.sort(nums);
+        int left = 0;
+        int right = nums[nums.length - 1] - nums[0];
+        int res = -1;
+
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (check(nums, mid, p)) {
+                res = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+
+    /**
+     * 小于等于 target 一共有多少个是否超多p个
+     * @param nums
+     * @param target
+     * @param p
+     * @param target
+     * @return
+     */
+    private boolean check(int[] nums, int target, int p) {
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i+1; j < nums.length; j++) {
+                if (nums[j] - nums[i] <= target) {
+                    count++;
+                } else {
+                    break;
+                }
+            }
+            if (count >= p) {
+                return true;
+            }
+        }
+        return count >= p;
+    }
+
+
+
+}
