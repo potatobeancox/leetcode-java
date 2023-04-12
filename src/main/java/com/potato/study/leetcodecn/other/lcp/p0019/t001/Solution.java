b03495582b5592f82a5ac5223a6f954e21aa0b90
liuzhao11 <liuzhao11@sankuai.com>
2023-04-12 22:45:39 +0800

    2023.4.10 剑指 Offer II 106. 二分图 2027.1.11

--- src/main/java/com/potato/study/leetcodecn/other/lcp/p0019/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/other/lcp/p0019/t001/Solution.java
@@ -57,8 +57,8 @@ public class Solution {
             }
             if (i > 0) {
                 dp[i][0] += dp[i-1][0];
-                dp[i][1] += dp[i-1][0] + dp[i-1][1];
-                dp[i][2] += dp[i-1][1] + dp[i-1][2];
+                dp[i][1] += Math.min(dp[i-1][0], dp[i-1][1]);
+                dp[i][2] += Math.min(dp[i-1][1], dp[i-1][2]);
             }
         }
         // 「红、黄、红 ryr 这种
