c9692206ce66b73450f480533d2b5fff4aab242b
liuzhao11 <liuzhao11@sankuai.com>
2023-04-29 18:01:14 +0800

    2023.4.15 2651. 计算列车到站时间 2027.1.26

---
+++ src/main/java/com/potato/study/leetcodecn/p02651/t001/Solution.java
@@ -0,0 +1,48 @@
+package com.potato.study.leetcodecn.p02651.t001;
+
+/**
+ *
+ * 2651. 计算列车到站时间
+ *
+ * 给你一个正整数 arrivalTime 表示列车正点到站的时间（单位：小时），另给你一个正整数 delayedTime 表示列车延误的小时数。
+
+ 返回列车实际到站的时间。
+
+ 注意，该问题中的时间采用 24 小时制。
+
+  
+
+ 示例 1：
+
+ 输入：arrivalTime = 15, delayedTime = 5
+ 输出：20
+ 解释：列车正点到站时间是 15:00 ，延误 5 小时，所以列车实际到站的时间是 15 + 5 = 20（20:00）。
+ 示例 2：
+
+ 输入：arrivalTime = 13, delayedTime = 11
+ 输出：0
+ 解释：列车正点到站时间是 13:00 ，延误 11 小时，所以列车实际到站的时间是 13 + 11 = 24（在 24 小时制中表示为 00:00 ，所以返回 0）。
+  
+
+ 提示：
+
+ 1 <= arrivaltime < 24
+ 1 <= delayedTime <= 24
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode.cn/problems/calculate-delayed-arrival-time
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ */
+public class Solution {
+
+    // 2651
+    public int findDelayedArrivalTime(int arrivalTime, int delayedTime) {
+        int target = arrivalTime + delayedTime;
+        target %= 24;
+        return target;
+    }
+
+
+
+}
