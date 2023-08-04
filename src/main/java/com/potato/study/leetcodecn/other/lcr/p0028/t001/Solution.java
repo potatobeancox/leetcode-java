a9dcad63f5e6e06daa4afa796f09d963ec95bbcd
liuzhao11 <liuzhao11@meituan.com>
2023-08-04 16:09:15 +0800

    2023.8.2 lcr 026 2027.6.4

--- src/main/java/com/potato/study/leetcodecn/other/lcr/p0028/t001/Solution.java
+++ src/main/java/com/potato/study/leetcodecn/other/lcr/p0028/t001/Solution.java
@@ -1,7 +1,7 @@
 package com.potato.study.leetcodecn.other.lcr.p0028.t001;
 
 /**
- * 剑指 Offer II 028. 展平多级双向链表
+ * LCR 028. 扁平化多级双向链表
  *
  * 多级双向链表中，除了指向下一个节点和前一个节点指针之外，它还有一个子链表指针，可能指向单独的双向链表。这些子列表也可能会有一个或多个自己的子项，依此类推，生成多级数据结构，如下面的示例所示。
  *
@@ -70,6 +70,8 @@ package com.potato.study.leetcodecn.other.lcr.p0028.t001;
  *  
  *
  * 注意：本题与主站 430 题相同： https://leetcode-cn.com/problems/flatten-a-multilevel-doubly-linked-list/
+ *
+ *
  *
  * 来源：力扣（LeetCode）
  * 链接：https://leetcode.cn/problems/Qv1Da2
